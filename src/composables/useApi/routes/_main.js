088fb37c4d0e25c623d7132c2f42b6c1e6f0971c
Andrei Priakhin <a.priakhin@finmars.com>
2022-06-09 23:11:01 +0400

    upd:090622

--- src/composables/useApi/routes/_main.js
+++ src/composables/useApi/routes/_main.js
@@ -3,8 +3,47 @@ let prefix = '/{client}/api/v1'
 export default {
 	pricingProc: {
 		get: prefix + "/procedures/pricing-procedure/",
+		post: prefix + "/procedures/pricing-procedure/{id}/run-procedure/",
+		put: '/procedures/pricing-procedure/{id}/'
+	},
+	pricingProcId: {
+		get: prefix + "/procedures/pricing-procedure/{id}/",
+		put: prefix + "/procedures/pricing-procedure/{id}/",
 	},
 	pricingProcInstance: {
 		get: prefix + "/procedures/pricing-parent-procedure-instance/",
-	}
-};
+	},
+
+	instrumentType: {
+		get: prefix + '/instruments/instrument-type/'
+	},
+	instrumentScheme: {
+		get: prefix + '/pricing/instrument-pricing-scheme/'
+	},
+	policyFilters: {
+		get: prefix + '/instruments/pricing-policy/'
+	},
+	portfolioFilters: {
+		get: prefix + '/portfolios/portfolio/'
+	},
+	currencyScheme: {
+		get: prefix + '/pricing/currency-pricing-scheme/'
+	},
+
+	importBankProc: {
+		get: prefix + '/procedures/request-data-procedure/'
+	},
+	importBankProcId: {
+		get: prefix + '/procedures/request-data-procedure/{id}/',
+		put: prefix + '/procedures/request-data-procedure/{id}/',
+	},
+
+	dataProvider: {
+		get: prefix + '/integrations/data-provider/'
+	},
+
+	dataInstance: {
+		get: prefix + '/procedures/data-procedure-instance/'
+	},
+
+}
