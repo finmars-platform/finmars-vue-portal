1a8eb6830b7f71cd3936960bce372cbe1f800d3a
Andrei Priakhin <a.priakhin@finmars.com>
2022-07-25 22:27:02 +0400

    fix

--- src/components/Fm/Card.vue
+++ src/components/Fm/Card.vue
@@ -1,20 +1,28 @@
 <template>
-	<div class="fm_card">
+	<div class="fm_card"
+		:class="{with_controls: controls}"
+	>
 		<div class="fm_card_title" v-if="title">{{ title }}</div>
 		<slot></slot>
+
+		<div class="fm_card_actions flex justify-end">
+			<slot name="controls"></slot>
+		</div>
 	</div>
 </template>
 
 <script setup>
 	defineProps({
-		title: String
+		title: String,
+		controls: Boolean
 	})
 </script>
 
 <style lang="scss" scoped>
 	.fm_card {
+		position: relative;
 		background: $separ;
-		box-shadow: 0 1px 4px rgb(103 102 102 / 25%);
+		box-shadow: 0 1px 4px hsl(0deg 0% 40% / 25%);
 		border-radius: 4px;
 		padding: 20px;
 		margin-bottom: $content-padding-x;
@@ -23,5 +31,17 @@
 			background: none;
 			box-shadow: none;
 		}
+	}
+	.fm_card_actions {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+		padding: inherit;
+		padding-bottom: 20px;
+		padding-top: 0;
+	}
+	.with_controls {
+		padding-bottom: 80px;
 	}
 </style>
