ce4060689dba3a2a980413cc3962da360546049a
mevstratov <m.evstratov@finmars.com>
2022-07-26 01:02:03 +0300

    WIP: profile -> tab BACKUPS

--- src/components/pages/profile/BackupsItem.vue
+++ src/components/pages/profile/BackupsItem.vue
@@ -5,26 +5,45 @@
 
 		<v-spacer></v-spacer>
 		<v-card-actions class="justify-space-between d-flex pa-4 py-2">
-			<v-btn v-if="!showActions" icon="mdi-lock" color="primary" @click="showActions = true"></v-btn>
 
-			<div v-if="showActions">
-				<v-btn target="download" :href="`${config.public.apiURL}/authorizer/master-user-backups/${backup.id}/view/`" icon="mdi-cloud-download" color="primary"></v-btn>
-				<v-btn icon="mdi-delete" color="primary" class="ml-0" @click="deleteBackup()"></v-btn>
+<!--			<v-btn v-if="!showActions" icon="mdi-lock" color="primary" @click="showActions = true"></v-btn>-->
+
+			<FmIcon v-if="!showActions" icon="lock" class="orange-text" @click="showActions = true"></FmIcon>
+
+			<div v-if="showActions" class="flex-row">
+<!--				<v-btn target="download" :href="`${config.public.apiURL}/authorizer/master-user-backups/${backup.id}/view/`" icon="mdi-cloud-download" color="primary"></v-btn>
+				<v-btn icon="mdi-delete" color="primary" class="ml-0" @click="deleteBackup()"></v-btn>-->
+				<a :href="`${config.public.apiURL}/authorizer/master-user-backups/${backup.id}/view/`" target="download">
+					<FmIcon icon="cloud_download" class="orange-text p-8"></FmIcon>
+				</a>
+
+				<FmIcon icon="delete"
+								class="orange-text m-l-0 p-8"
+								@click="deleteBackup()"></FmIcon>
 			</div>
 
-			<v-btn color="primary">
+<!--			<v-btn color="primary">
 				restore
 				<PagesProfileRestoreFromBackup
 					@close="isShowRestore = false, emit('refresh')"
 					v-model="isShowRestore"
 					activator="parent"
 				/>
-			</v-btn>
+			</v-btn>-->
+			<FmBtn color="primary-text">
+				RESTORE
+				<PagesProfileRestoreFromBackup
+					@close="isShowRestore = false, emit('refresh')"
+					v-model="isShowRestore"
+					activator="parent"
+				/>
+			</FmBtn>
 		</v-card-actions>
 	</v-card>
 </template>
 
 <script setup>
+
 	const emit = defineEmits(["refresh"]);
 	const props = defineProps({
 		backup: Object,
