94fda3cd0611378bf6e5de2ba99e32230fbaa70c
Sergei Zhitenev <s.zhitenev@finmars.com>
2022-09-07 19:09:15 +0200

    update docker

--- docker/substitute_environment_variables.sh
+++ docker/substitute_environment_variables.sh
@@ -8,7 +8,7 @@ echo PROD_WS_URL $PROD_WS_URL
 echo PROD_OLD_APP_URL $PROD_OLD_APP_URL
 
 # Replace env vars in files served by server
-for file in $ROOT_DIR/.output/**/*.mjs* $ROOT_DIR/.output/public/index.html;
+for file in $ROOT_DIR/**/*.mjs*
 do
   sed -i 's|PROD_APP_URL|'${PROD_APP_URL}'|g' $file
   sed -i 's|PROD_API_URL|'${PROD_API_URL}'|g' $file
