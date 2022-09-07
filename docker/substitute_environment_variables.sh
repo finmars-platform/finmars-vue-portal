0856e0445b3b08fe28114d9e0872f06b9570c863
Sergey Zhitenev <szhitenev@szhitenev-macbookpro.local>
2022-09-07 19:11:50 +0200

    update docker

--- docker/substitute_environment_variables.sh
+++ docker/substitute_environment_variables.sh
@@ -8,7 +8,7 @@ echo PROD_WS_URL $PROD_WS_URL
 echo PROD_OLD_APP_URL $PROD_OLD_APP_URL
 
 # Replace env vars in files served by server
-for file in $ROOT_DIR/**/*.mjs*
+for file in $ROOT_DIR/.nuxt/**/*.mjs* $ROOT_DIR/.output/**/*.mjs*
 do
   sed -i 's|PROD_APP_URL|'${PROD_APP_URL}'|g' $file
   sed -i 's|PROD_API_URL|'${PROD_API_URL}'|g' $file
