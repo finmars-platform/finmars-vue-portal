56bbc5239e43a378b996177863f5b707bafd9157
Andrei Priakhin <a.priakhin@finmars.com>
2022-09-08 18:38:04 +0400

    fix

---
+++ src/components/Fm/SelectWindow.vue
@@ -0,0 +1,183 @@
+<template>
+	<div class="flex sp aic">
+		<div class="available">
+			<div class="header">Available</div>
+
+			<div class="block">
+				<div class="search">
+					<BaseInput class="m-b-0"
+						label="Search"
+						v-model="availableSearch"
+					/>
+				</div>
+				<div class="list">
+					<div class="list_item"
+						v-for="item in availableList"
+						:key="item.name"
+						:class="{selected: item.selected}"
+						@click="item.selected = !item.selected"
+					>
+						{{ item[item_title] }}
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="actions">
+			<FmIcon btn icon="chevron_right" @click="addItem()" />
+			<FmIcon btn icon="keyboard_double_arrow_right" @click="addItem('all')" />
+
+			<FmIcon btn icon="chevron_left" @click="removeItem()" />
+			<FmIcon btn icon="keyboard_double_arrow_left" @click="removeItem('all')" />
+		</div>
+
+		<div class="selected">
+			<div class="header">Selected</div>
+
+			<div class="block">
+				<div class="search">
+					<BaseInput class="m-b-0"
+						label="Search"
+						v-model="selectedSearch"
+					/>
+				</div>
+				<div class="list">
+					<div class="list_item"
+						v-for="item in selectedList"
+						:key="item.name"
+						:class="{selected: item.selected}"
+						@click="item.selected = !item.selected"
+					>
+						{{ item[item_title] }}
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	let props = defineProps({
+		items: Array,
+		modelValue: [String, Array],
+		item_title: {
+			type: String,
+			default: 'user_code'
+		}
+	})
+	let emit = defineEmits(['update:modelValue'])
+
+	let isOpen = ref(false)
+	let availableSearch = ref('')
+	let selectedSearch = ref('')
+
+	let modelValueArray = props.modelValue
+
+	if ( typeof modelValueArray == 'string' ) modelValueArray = modelValueArray.split(',')
+
+	let selectedFilter = reactive( new Set( modelValueArray ) )
+
+	let selectedList  = computed(() => {
+		if ( !props.items.length ) return []
+
+		return props.items.filter(
+			item => {
+				let elem = item[props.item_title]
+
+				return selectedFilter.has(elem) &&
+					elem.toLocaleLowerCase().includes( selectedSearch.value.toLocaleLowerCase() )
+			}
+		)
+	})
+
+	let availableList = computed(() => {
+		if ( !props.items.length ) return []
+
+		return props.items.filter(
+			item => {
+				let elem = item[props.item_title]
+
+				return !selectedFilter.has(elem) &&
+					elem.toLocaleLowerCase().includes(availableSearch.value.toLocaleLowerCase())
+			}
+		)
+	})
+
+	function save() {
+		let result = [...selectedFilter]
+		if ( typeof props.modelValue == 'String' ) result = result.join(',')
+
+		emit('update:modelValue', result )
+
+		isOpen.value = false
+	}
+	function addItem( all ) {
+		let items = availableList.value
+			.filter(item => item.selected || !!all)
+			.map( item => {
+				item.selected = false
+				return item[props.item_title]
+			})
+		items.forEach( item => selectedFilter.add(item) )
+	}
+	function removeItem( all ) {
+		let items = selectedList.value
+			.filter(item => item.selected || !!all)
+			.map( item => {
+				item.selected = false
+				return item[props.item_title]
+			})
+
+		items.forEach( item => selectedFilter.delete(item) )
+	}
+
+</script>
+
+<style lang="scss" scoped>
+	.ms_wrap {
+		cursor: pointer;
+	}
+	.fm_chip {
+		background: $main-darken-2;
+		padding: 3px 8px;
+		border-radius: 16px;
+		min-width: 30px;
+		text-align: center;
+
+		& + & {
+			margin-left: 5px;
+		}
+	}
+	.header {
+		font-size: 20px;
+		margin-bottom: 8px;
+	}
+	.block {
+		min-width: 400px;
+	}
+	.list {
+		border-bottom: 1px solid $border-darken;
+		border-left: 1px solid $border-darken;
+		border-right: 1px solid $border-darken;
+		padding: 10px 0;
+		overflow: auto;
+		height: 270px;
+	}
+	.list_item {
+		padding: 7px 14px;
+		cursor: pointer;
+
+		&.selected {
+			background: #eee;
+		}
+
+		& + & {
+			border-top: 1px solid #fff;
+		}
+	}
+	.search {
+	}
+	.actions {
+		padding: 0 10px;
+	}
+</style>
--- src/pages/reports/performance.vue
+++ src/pages/reports/performance.vue
@@ -1,11 +1,12 @@
 <template>
 	<div>
-		<PagesReportsPerformanceDialogSettings v-model:open-dialog="showSettingsDialog"
-																					 v-model:layout-ready-status="layoutSet"
-																					 :viewer-data="viewerData"
-																					 @save="showSettingsDialog = false;"
-																					 @cancel="showSettingsDialog = false;" />
-
+		<PagesReportsPerformanceDialogSettings
+			v-model:open-dialog="showSettingsDialog"
+			v-model:layout-ready-status="layoutSet"
+			:viewer-data="viewerData"
+			@save="showSettingsDialog = false;"
+			@cancel="showSettingsDialog = false;"
+		/>
 
 		<EvBaseTopPanel height="50" @open-settings="showSettingsDialog = true;" />
 
@@ -20,16 +21,27 @@
 						<div class="fm_list_item">
 							Add Portfolio register
 						</div>
-						<div class="fm_list_item">
+						<div class="fm_list_item" @click="isOpenAddBundle = true">
 							Add bundle
 						</div>
 					</div>
 				</FmMenu>
+				<BaseModal v-model="isOpenAddBundle"
+					@cancel="isOpenAddBundle = false">
 
+					<BaseInput v-model="newBundle.name" />
+					<FmSelectWindow v-model="newBundle.registers" :items="registersItems"></FmSelectWindow>
+					<template #controls>
+						<div class="flex sb">
+							<FmBtn type="text" @click="isOpenAddBundle = false">cancel</FmBtn>
+							<FmBtn @click="createBundle()">create</FmBtn>
+						</div>
+					</template>
+				</BaseModal>
 			</template>
 
 			<template #rightActions>
-				<FmIcon icon="refresh" btn />
+				<FmIcon icon="refresh" @click="init()" btn />
 			</template>
 		</FmHorizontalPanel>
 
@@ -92,9 +104,38 @@
 
 	let showSettingsDialog = ref(false);
 
-	// <editor-fold desc="Variables">
+	let isOpenAddBundle = ref(false)
+	let newBundle = ref({
+		registers: []
+	})
+	let registersItems = ref([])
+
+	useApi('portfolioRegister.post', {
+		body: '{"groups_types":[],"page":1,"groups_values":[],"groups_order":"asc","page_size":60,"ev_options":{"entity_filters":["enabled","disabled","active","inactive"]},"filter_settings":[],"global_table_search":"","is_enabled":"any"}'
+	}).then((res) => {
+		res.results.forEach((item) => {
+			registersItems.value.push({
+				user_code: item.user_code,
+				id: item.id,
+			})
+		})
+	})
+
+	async function createBundle() {
+		let res = await useApi('portfolioBundles.post', {body: newBundle.value})
+
+		if ( res ) {
+			newBundle.value = {
+				registers: []
+			}
+
+			init()
+		}
+	}
+
+	// #region Main
 	let panels = ref(['period', 'detail', 'diagram'])
-	let porfolios = []
+	let bundles = []
 	let preriodHeaders = ref(
 		['', 'Daily', 'MTD', 'QTD', 'YTD', +moment().year() - 1, +moment().year() - 2, 'Incept']
 	)
@@ -116,7 +157,7 @@
 
 	let layoutSet = ref(false);
 
-	// </editor-fold>
+	/* #endregion */
 
 	async function choosePortfolio(id) {
 		activePeriod.value = id
@@ -159,10 +200,10 @@
 
 		await fetchPortolios()
 
-		if ( !porfolios.length ) {
+		if ( !bundles.length ) {
 			return false
 		}
-		detailPortfolio.value = porfolios[0].user_code
+		detailPortfolio.value = bundles[0].user_code
 
 		await getMonthDetails()
 
@@ -208,45 +249,44 @@
 	}
 
 	async function fetchPortolios() {
-		let res = await useApi('portfolioRegister.post', {
-			body: '{"groups_types":[],"page":1,"groups_values":[],"groups_order":"asc","page_size":60,"ev_options":{"entity_filters":["enabled","disabled","active","inactive"]},"filter_settings":[],"global_table_search":"","is_enabled":"any"}'
-		})
-		porfolios = res.results
+		let res = await useApi('portfolioBundles.get')
+		bundles = res.results
+		preriodItems.value = []
 
-		porfolios.forEach( portfolio => {
+		bundles.forEach( bundle => {
 			preriodItems.value.push({
-				name: portfolio.user_code,
+				name: bundle.user_code,
 			})
 
 			let row = preriodItems.value[ preriodItems.value.length - 1 ]
 
 			row.daily = null
-			getDay( [portfolio.linked_instrument] ).then(day => {
+			getDay( bundle.id ).then(day => {
 				row.daily = Math.round(day * 100 * 100) / 100 + '%'
 			})
 
 			row.month = null
-			getMonth( [portfolio.linked_instrument] ).then(month => {
+			getMonth( bundle.id ).then(month => {
 				row.month = Math.round(month * 100 * 100) / 100 + '%'
 			})
 
 			row.q = null
-			getQ( [portfolio.linked_instrument] ).then(q => {
+			getQ( bundle.id ).then(q => {
 				row.q = Math.round(q * 100 * 100) / 100 + '%'
 			})
 
 			row.year = null
-			getYear( [portfolio.linked_instrument] ).then(year => {
+			getYear( bundle.id ).then(year => {
 				row.year = Math.round(year * 100 * 100) / 100 + '%'
 			})
 
 			row.last = null
-			getLastYear( [portfolio.linked_instrument] ).then(last => {
+			getLastYear( bundle.id ).then(last => {
 				row.last = Math.round(last * 100 * 100) / 100 + '%'
 			})
 
 			row.beforeLast = null
-			getYearBeforeLast( [portfolio.linked_instrument] ).then(beforeLast => {
+			getYearBeforeLast( bundle.id ).then(beforeLast => {
 				row.beforeLast = Math.round(beforeLast * 100 * 100) / 100 + '%'
 			})
 
@@ -259,9 +299,9 @@
 		portfolioItems.value = []
 		portfolioItemsCumm.value = []
 
-		let instr_id = name
-			? porfolios.find(item => item.name == name).linked_instrument
-			: porfolios[0].linked_instrument
+		let bundleId = name
+			? bundles.find(item => item.name == name).id
+			: bundles[0].id
 
 		let allMonths = await useApi('performanceReport.post', {
 			body: {
@@ -270,7 +310,7 @@
 				"end_date": null,
 				"calculation_type": "time_weighted",
 				"segmentation_type": 'months',
-				"registers": [ instr_id ]
+				"bundle": bundleId
 			}
 		})
 
@@ -380,7 +420,7 @@
 		return await getReports({start, end, ids})
 	}
 
-	async function getReports({start, end, ids, type = 'months'}) {
+	async function getReports({start, end, id, type = 'months'}) {
 		let res = await useApi('performanceReport.post', {
 			body: {
 				 "save_report": false,
@@ -388,7 +428,7 @@
 				"end_date": end,
 				"calculation_type": "time_weighted",
 				"segmentation_type": type,
-				"registers": ids
+				"bundle": id
 			}
 		})
 
