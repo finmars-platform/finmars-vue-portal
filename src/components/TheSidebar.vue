39fd2820bc37aa0362535135f7fa57df67a9dfb6
Andrei Priakhin <a.priakhin@finmars.com>
2022-09-12 18:25:55 +0400

    fix

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -4,7 +4,7 @@
 			<div class="sidenav-logo-container">
 				<div class="sidenav-logo-wrapper">
 					<NuxtLink to="/">
-						<img src="/v/img/logo.png" class="sidenav-logo" alt="" />
+						<img src="/img/logo.png" class="sidenav-logo" alt="" />
 					</NuxtLink>
 				</div>
 				<div class="panel-resizer-holder sidenav-resizer-holder">
