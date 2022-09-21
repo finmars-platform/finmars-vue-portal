fc3667e5d7b08c94102f70d2c34453dca3506c3f
Andrei Priakhin <a.priakhin@finmars.com>
2022-09-21 12:36:41 +0400

    Chart upd

--- src/pages/widgets/barchart.vue
+++ src/pages/widgets/barchart.vue
@@ -5,7 +5,60 @@
 </template>
 
 <script setup>
-	import Chart from 'chart.js/auto';
+	import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+} from 'chart.js';
+
+	Chart.register(
+		ArcElement,
+		LineElement,
+		BarElement,
+		PointElement,
+		BarController,
+		BubbleController,
+		DoughnutController,
+		LineController,
+		PieController,
+		PolarAreaController,
+		RadarController,
+		ScatterController,
+		CategoryScale,
+		LinearScale,
+		LogarithmicScale,
+		RadialLinearScale,
+		TimeScale,
+		TimeSeriesScale,
+		Decimation,
+		Filler,
+		Legend,
+		Title,
+		Tooltip,
+		SubTitle
+	);
 
 	definePageMeta({
 		layout: 'auth'
@@ -13,10 +66,13 @@
 
 	let wId = useRoute().query.wId
 
+	let res = await useApi('widgetsHistory.get')
+	let active = ref(null)
+
 	onMounted(() => {
 		initPostMessageBus()
 
-		let chart = new Chart('myChart', {
+		let myChart = new Chart('myChart', {
 			type: 'bar',
 			data: {
 				labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
@@ -24,21 +80,54 @@
 					{
 						label: 'Crypto',
 						data: [-1,2,3,5,3,2,4,5,3,6,12,3],
-						backgroundColor: '#43AA8B',
+						backgroundColor: 'rgba(87, 117, 144, 0.8)',
 					},
 					{
 						label: 'Cash & Equivalents',
 						data: [4,5,3,5,3,6,4,5,9,4,11,3],
-						backgroundColor: '#FA6769',
+						backgroundColor: 'rgba(67, 170, 139, 0.8)',
 					},
 					{
 						label: 'Monthly Returns 3',
 						data: [1,3,1,3,-9,8,4,5,3,6,3,3],
-						backgroundColor: '#577590',
+						backgroundColor: 'rgba(249, 199, 79, 0.8)',
+					},
+					{
+						label: 'test 3',
+						data: [1,3,1,3,-9,8,4,5,3,6,3,3],
+						backgroundColor: 'rgba(151, 155, 255, 0.8)',
+					},
+					{
+						label: 'Monthly mi',
+						data: [1,3,1,3,-9,8,4,5,3,6,3,3],
+						backgroundColor: 'rgba(250, 103, 105, 0.8)',
 					}
 				],
 			},
+			plugins: [{
+				id: 'custom_canvas_background_color',
+				afterDraw: (chart, args, options) => {
+					if ( !active.value ) return;
+					const {ctx} = chart;
+					ctx.save();
+					ctx.globalCompositeOperation = 'destination-over';
+					ctx.fillStyle = options.color;
+					ctx.fillRect(
+						active.value.element.x - active.value.element.width * 1.25 / 2,
+						chart.chartArea.top,
+						active.value.element.width * 1.25,
+						chart.chartArea.height
+					);
+					ctx.restore();
+				},
+				defaults: {
+					color: 'rgba(243, 123, 78, 0.2)'
+				}
+			}],
 			options: {
+				barPercentage: 0.8,
+				categoryPercentage: 1.0,
+				borderRadius: 3,
 				responsive: true,
 				maintainAspectRatio: false,
 				scales: {
@@ -46,13 +135,27 @@
 						stacked: true,
 					},
 					y: {
-						stacked: true
+						stacked: true,
+						position: 'right',
+						grid: {
+							tickWidth: 1,
+							tickLength: 1,
+						}
 					}
 				},
 				plugins: {
 					legend: {
-						position: 'top',
-						reverse: true
+						position: 'bottom',
+						align: 'start',
+						labels: {
+							padding: 20,
+							color: '#404040',
+							boxWidth: 26,
+							boxHeight: 26,
+							font: {
+								size: 16
+							}
+						}
 					}
 				},
 				interaction: {
@@ -60,12 +163,14 @@
 					axis: 'x'
 				},
 				onClick: (evt) => {
-					const points = chart.getElementsAtEventForMode(evt, 'nearest', { intersect: false, axis: 'x' }, true);
+					const points = myChart.getElementsAtEventForMode(evt, 'nearest', { intersect: false, axis: 'x' }, true);
 
 					if (points.length) {
 						const firstPoint = points[0];
-						const label = chart.data.labels[firstPoint.index];
-						const value = chart.data.datasets[firstPoint.datasetIndex].data[firstPoint.index];
+						const label = myChart.data.labels[firstPoint.index];
+						const value = myChart.data.datasets[firstPoint.datasetIndex].data[firstPoint.index];
+						active.value = firstPoint
+						myChart.update()
 
 						send({
 							action: 'clickOnChart',
