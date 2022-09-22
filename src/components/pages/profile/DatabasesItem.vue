27ddd1c515a123932e2d6810826383d9d89428cf
Andrei Priakhin <a.priakhin@finmars.com>
2022-09-22 13:31:26 +0400

    Profile fix

--- src/components/pages/profile/DatabasesItem.vue
+++ src/components/pages/profile/DatabasesItem.vue
@@ -5,7 +5,7 @@
 		<div class="flex aic sb">
 			<div class="fm_card_title edit_hover m-b-0">
 				<span v-if="!isEditTitle">
-					{{ isEditTitle ? editingData.name : db.name }}
+					{{ isEditTitle ? editingData.name : db.name.length > 20 ? db.name.slice(0, 20) + '...' : db.name }}
 				</span>
 				<FmIcon primary
 					v-if="!isEditTitle"
