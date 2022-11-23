d6928a891bac726ce57d25ba2bf4c6411320fcf6
Andrei Priakhin <a.priakhin@finmars.com>
2022-11-24 01:45:02 +0400

    FN-993

--- docker/substitute_environment_variables.sh
+++ docker/substitute_environment_variables.sh
@@ -2,18 +2,23 @@
 
 ROOT_DIR=/var/www/finmars
 
-echo PROD_APP_URL $PROD_APP_URL
-echo PROD_API_URL $PROD_API_URL
-echo PROD_WS_URL $PROD_WS_URL
-echo PROD_OLD_APP_URL $PROD_OLD_APP_URL
+echo PROD_FRONT_HOST $PROD_FRONT_HOST
+echo NUXT_APP_BASE_URL $NUXT_APP_BASE_URL
+echo PROD_API_HOST $PROD_API_HOST
+echo PROD_WS_HOST $PROD_WS_HOST
+
+echo PROD_CLOACK_PASS $PROD_CLOACK_PASS
+echo PROD_CLOACK_2fa $PROD_CLOACK_2fa
 
 # Replace env vars in files served by server
 for file in $ROOT_DIR/.output/**/*.mjs* $ROOT_DIR/.output/server/chunks/nitro/node-server.mjs
 do
-  sed -i 's|==PROD_APP_URL==|'${PROD_APP_URL}'|g' $file
-  sed -i 's|==PROD_API_URL==|'${PROD_API_URL}'|g' $file
-  sed -i 's|==PROD_WS_URL==|'${PROD_WS_URL}'|g' $file
-  sed -i 's|==PROD_OLD_APP_URL==|'${PROD_OLD_APP_URL}'|g' $file
+  sed -i 's|==PROD_FRONT_URL==|'${PROD_FRONT_URL}'|g' $file
+  sed -i 's|==NUXT_APP_BASE_URL==|'${NUXT_APP_BASE_URL}'|g' $file
+  sed -i 's|==PROD_API_HOST==|'${PROD_API_HOST}'|g' $file
+  sed -i 's|==PROD_WS_HOST==|'${PROD_WS_HOST}'|g' $file
+  sed -i 's|==PROD_CLOACK_PASS==|'${PROD_CLOACK_PASS}'|g' $file
+  sed -i 's|==PROD_CLOACK_2fa==|'${PROD_CLOACK_2fa}'|g' $file
 done
 
 #grep -rnw '/var/www/finmars/' -e 'PROD_APP_URL'
