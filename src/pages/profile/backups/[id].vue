bbb51e7ef39c0df6b199bb88e7de9ccc10ff706c
Sergey Zhitenev <s.zhitenev@finmars.com>
2023-01-31 14:33:43 +0000

    FN-1211 update table

--- src/pages/profile/backups/[id].vue
+++ src/pages/profile/backups/[id].vue
@@ -150,8 +150,8 @@
 			backupsByMU.value.push({
 				name: item.name,
 				date: dayjs(item.created_at).format('DD MMM YYYY HH:mm'),
-				status: 'hz',
-				created_by: item.created_by,
+				status: item.status,
+				created_by: item.created_by.username,
 				file_size: Math.round(item.file_size / 1024) + ' KB',
 				notes: item.notes,
 			})
