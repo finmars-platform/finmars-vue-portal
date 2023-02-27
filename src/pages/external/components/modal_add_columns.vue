621e82e4904a7ea007083112bdc66ed1f78f3dab
Andrey Priakhin <a.priakhin@finmars.com>
2023-02-28 00:30:00 +0400

    FN-753

--- src/pages/external/components/modal_add_columns.vue
+++ src/pages/external/components/modal_add_columns.vue
@@ -10,10 +10,6 @@
 					:modelValue="searchParam"
 					@update:modelValue="search"
 				>
-				<!--
-					v-model="searchParam"
-					:modelValue="searchParam"
-					@update:modelValue="search" -->
 					<template #button>
 						<FmIcon icon="search" />
 					</template>
@@ -47,104 +43,180 @@
 				</div>
 
 				<div
-					class="fm_tabs_item"
+					class="fm_tabs_item flex aic"
 					:class="{active: tab == 'selected'}"
 					@click="tab = 'selected'"
 				>
 					Selected
+					<div class="select_count">
+						{{  selectedColumns.length }}
+					</div>
 				</div>
 			</div>
 
 			<div :class="[isAdvanced && tab == 'advanced' ? 'advanced' : '', 'content_grid']">
-				<div class="content_grid_left" v-if="isAdvanced && tab == 'advanced'">
+				<div class="content_grid_left" v-show="isAdvanced && tab == 'advanced'">
 					<ul class="fm_list">
-						<li class="fm_list_item attr_item" :class="{active: activeTree == prop}" v-for="(val, prop) in advancedColumns" @click="activeTree = prop">{{ prop }}</li>
+						<li
+							class="fm_list_item attr_item"
+							:class="{active: activeTree == i}"
+							v-for="(item, i) in advancedColumns"
+							@click="activeTree = i"
+						>
+							{{ item.name }}
+						</li>
 					</ul>
 				</div>
 
 				<div class="content_grid_main">
-					<div class="fm_list" v-if="!isAdvanced || tab == 'favorites'">
+					<div class="fm_list" v-show="!isAdvanced || tab == 'favorites'">
 						<div
 							class="fm_list_item attr_item flex aic sb"
 							:class="{active: activeRow == item.key}"
-							v-for="(item, i) in filteredColumns"
+							v-for="(item, i) in favoritesTab"
 							@click="activeRow = item.key"
-							@dblclick="item.checked = !item.checked"
+							@dblclick="selected[item.key] = !selected[item.key]"
 						>
 							<div class="flex aic">
 								<FmCheckbox
-									v-model="item.checked"
-									@click.stop="() => {}"
+									v-model="selected[item.key]"
+									@click.stop=""
 								/>
 
-								<div v-html="item.name"></div>
+								<div v-html="favList[item.key]"></div>
 							</div>
-
-							<FmIcon v-if="tab == 'advanced'" class="favorites" size="20" primary icon="star_outlined" />
 						</div>
 					</div>
-					<template v-else>
-						<ul class="fm_list">
-							<template
-								v-for="(obj, prop) in advancedColumns[activeTree]"
+
+					<div class="fm_list" v-show="tab == 'selected'">
+						<li class="fm_list_item attr_item"
+							@click="isOpenSelect.current = !isOpenSelect.current"
+						>
+							<FmIcon class="expand" :icon="isOpenSelect.current ? 'expand_less' : 'expand_more'" />
+							<div>Current</div>
+						</li>
+						<div v-show="isOpenSelect.current">
+							<div
+								class="fm_list_item attr_item flex aic sb "
+								:class="{active: activeRow == item.key}"
+								v-for="(item, i) in selectedOld"
+								:key="item.key"
+								@click="activeRow = item.key"
 							>
-								<div
-									v-if="obj.key"
-									class="fm_list_item attr_item flex aic sb"
-									:class="{active: activeRow == obj.key}"
-									@click="activeRow = obj.key"
-									@dblclick="obj.checked = !obj.checked"
-								>
-									<div class="flex aic">
-										<FmCheckbox
-											v-model="obj.checked"
-											@click.stop="() => {}"
-										/>
+								<div class="flex aic expand_wrap">
+									<FmCheckbox
+										disabled
+									/>
 
-										<div v-html="prop"></div>
-									</div>
+									<div class="select_old" v-html="`[${item.name}]`"></div>
+								</div>
+							</div>
+						</div>
 
-									<FmIcon v-if="tab == 'advanced'" class="favorites" size="20" primary icon="star_outlined" />
+						<li class="fm_list_item attr_item"
+							@click="isOpenSelect.new = !isOpenSelect.new"
+						>
+							<FmIcon class="expand" :icon="isOpenSelect.new ? 'expand_less' : 'expand_more'" />
+							<div>New</div>
+						</li>
+						<div v-show="isOpenSelect.new">
+							<div
+								class="fm_list_item attr_item flex aic sb"
+								:class="{active: activeRow == item.key}"
+								v-for="(item, i) in selectedColumns"
+								:key="item.key"
+								@click="activeRow = item.key"
+								@dblclick="selected[item.key] = !selected[item.key]"
+							>
+								<div class="flex aic expand_wrap">
+									<FmCheckbox
+										v-model="selected[item.key]"
+										@click.stop="() => {}"
+									/>
+
+									<div v-html="item.name"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<ul class="fm_list" v-show="tab == 'advanced'">
+						<template
+							v-for="obj in advancedColumnActive"
+						>
+							<div
+								v-if="obj.key"
+								class="fm_list_item attr_item flex aic sb"
+								:class="{active: activeRow == obj.key}"
+								@click="activeRow = obj.key"
+								@dblclick="selected[obj.key] = !selected[obj.key]"
+							>
+								<div class="flex aic">
+									<FmCheckbox
+										v-model="selected[obj.key]"
+										@click.stop=""
+									/>
+
+									<div v-html="obj.short_name"></div>
 								</div>
 
-								<div v-else>
-									<li class="fm_list_item attr_item" @click="obj.isOpen = !obj.isOpen">
-										<FmIcon size="24" icon="expand_more" />
-										<div v-html="prop"></div>
-									</li>
+								<FmIcon
+									size="20"
+									primary
+									:class="['favorites', {active: !!favList[obj.key]}]"
+									:icon="favList[obj.key] ? 'star' : 'star_outlined'"
+									@click="favList[obj.key] = obj.name"
+								/>
+							</div>
 
-									<template v-if="obj.isOpen">
-										<div
-											v-for="(val, prop) in obj"
-											class="fm_list_item attr_item flex aic sb"
-											:class="{active: activeRow == val.key}"
-											@click="activeRow = val.key"
-											@dblclick="val.checked = !val.checked"
-											v-if="prop != 'isOpen'"
-										>
-											<div class="flex aic p-l-24">
-												<FmCheckbox
-													v-model="val.checked"
-													@click.stop="() => {}"
-												/>
+							<div v-else>
+								<li class="fm_list_item attr_item" @click="obj.isOpen = !obj.isOpen">
+									<FmIcon class="expand" icon="expand_more" />
+									<div v-html="obj.name"></div>
+								</li>
 
-												<div v-html="prop"></div>
-											</div>
+								<template v-if="obj.isOpen">
+									<div
+										v-for="(val, prop) in obj.children"
+										class="fm_list_item attr_item flex aic sb"
+										:class="{active: activeRow == val.key}"
+										@click="activeRow = val.key"
+										@dblclick="selected[val.key] = !selected[val.key]"
+									>
+										<div class="flex aic expand_wrap">
+											<FmCheckbox
+												v-model="selected[val.key]"
+												@click.stop=""
+											/>
 
-											<FmIcon v-if="tab == 'advanced'" class="favorites" size="20" primary icon="star_outlined" />
+											<div v-html="val.short_name"></div>
 										</div>
-									</template>
 
-								</div>
-							</template>
-						</ul>
-					</template>
+										<FmIcon
+											size="20"
+											primary
+											:class="['favorites', {active: !!favList[val.key]}]"
+											:icon="favList[val.key] ? 'star' : 'star_outlined'"
+											@click="favList[val.key] = val.name"
+										/>
+									</div>
+								</template>
+
+							</div>
+						</template>
+					</ul>
 				</div>
 
 				<div class="content_grid_right">
-					<div class="desc_title">{{ favorites.name }}</div>
-					<div class="desc_subtitle">{{ favorites.path }}</div>
-					<div class="desc_about">{{ favorites.info }}</div>
+					<div class="flex aic" v-if="tab != 'advanced'">
+						<div class="desc_title" v-if="!isEdit">{{ attrInfo.name }}</div>
+						<BaseInput v-else v-model="favList[attrInfo.key]" />
+
+						<FmIcon v-if="!isEdit" size="20" primary icon="edit" @click="isEdit = true" />
+						<FmIcon v-else primary icon="save" @click="isEdit = false" />
+					</div>
+					<div class="desc_subtitle" v-if="tab != 'advanced'">[{{ attrInfo.path }}]</div>
+					<div class="desc_about">{{ attrInfo.info }}</div>
 				</div>
 			</div>
 		</div>
@@ -183,7 +255,12 @@
 	let isAdvanced = ref(false)
 	let tab = ref('favorites')
 	let activeRow = ref('')
-	let activeTree = ref('')
+	let activeTree = ref(0)
+	let isEdit = ref(false)
+	let isOpenSelect = reactive({
+		current: true,
+		new: true
+	})
 
 	const formatedAttrs = attributes.map(item => {
 		item.name = item.name.replaceAll('. ', ' / ')
@@ -197,7 +274,21 @@
 		return a.name.localeCompare(b.name)
 	})
 
+	const selectedOldProp = ['account.attributes.asset_type', 'account.attributes.account_type', 'account.attributes.asset_class']
+
+	let selectedOld = formatedAttrs.filter(item => selectedOldProp.includes(item.key))
+
 	activeRow.value = formatedAttrs[0].key
+
+	// let selectedColumns = ref([])
+	let selected = reactive({})
+	const selectedColumns = computed(() => {
+		console.log('selected:', selected)
+		return filteredColumns.value.filter((item) => selected[item.key])
+	})
+	function filterSelected() {
+		selectedColumns.value = filteredColumns.value.filter((item) => selected.value[item.key])
+	}
 
 	const filteredColumns = computed(() => {
 		if ( !searchParam.value ) return formatedAttrs
@@ -221,13 +312,21 @@
 				(match) => `<span class="c_primary">${match}</span>`
 			)
 
-			return { name, key: item.key, checked: ref(false) }
+			return reactive({ name, key: item.key, checked: false })
 		})
 
 		return result
 	})
+	const advancedColumnActive = computed(() => {
+		let result = []
+
+		if ( advancedColumns.value[activeTree.value] )
+			result = advancedColumns.value[activeTree.value].children
+
+		return result
+	})
 	const advancedColumns = computed(() => {
-		let tree = {};
+		let tree = {}
 
 		for (let attr of filteredColumns.value) {
 			const parts = attr.name.split(" / ")
@@ -240,29 +339,70 @@
 
 				if (!node[part]) {
 					if ( parts.length - 1 == i ) {
+						attr.short_name = part
 						node[part] = attr
 
 					} else {
 
-						node[part] = reactive({})
+						node[part] = {}
 
-						if ( i > 0 ) node[part].isOpen = false
+						node[part].isOpen = false
 					}
 				}
 				node = node[part]
 			}
 		}
-		console.log('tree:', tree)
+
+		tree = iterateNestedObject(tree)
 
 		return tree
 	})
-	const favorites = computed(() => {
+	function iterateNestedObject( obj ) {
+		let list = []
+
+		for (let key in obj) {
+			let newObj = reactive({
+				name: key,
+				children: []
+			})
+
+			if ( obj[key].isOpen === false) {
+				newObj.isOpen = ref(false)
+				newObj.children = iterateNestedObject(obj[key])
+
+			} else {
+
+				newObj = obj[key]
+			}
+
+			if ( key != 'isOpen')
+				list.push(newObj)
+		}
+
+		return list
+	}
+	let favList = reactive({
+		'pricing_currency.reference_for_pricing': 'test'
+	})
+	let attrMetas = reactive({
+		key: 'pricing_currency.reference_for_pricing',
+		name: 'test',
+		description: 'Info'
+	})
+	const favoritesTab = computed(() => {
+		let attrs = formatedAttrs.filter(item => favList[item.key] != undefined )
+		if ( !attrs ) return {}
+
+		return attrs
+	})
+	const attrInfo = computed(() => {
 		let attr = formatedAttrs.find(item => item.key == activeRow.value)
 		if ( !attr ) return {}
 
 		return {
-			name: activeRow.value,
+			name: favList[attr.key] || attr.name,
 			path: attr.name,
+			key: attr.key,
 			info: 'No info',
 		}
 	})
@@ -273,7 +413,7 @@
 		clearTimeout(searchTimer)
 
 		searchTimer = setTimeout(() => {
-			searchParam.value = value
+			searchParam.value = value.trim()
 		}, 300)
 	}
 
@@ -323,6 +463,18 @@
 			font-weight: 500;
 			font-size: 20px;
 		}
+	}
+	.select_count {
+		background: $primary;
+		width: 18px;
+		height: 18px;
+		line-height: 18px;
+		text-align: center;
+		color: #fff;
+		margin-left: 11px;
+		font-size: 12px;
+		font-weight: 400;
+		border-radius: 2px;
 	}
 	.content_grid {
 		display: grid;
@@ -387,5 +539,19 @@
 	.favorites {
 		opacity: 0;
 		transition: 0.3s;
+
+		&.active {
+			opacity: 1;
+		}
+	}
+	.expand_wrap {
+		padding-left: 31px;
+	}
+	.expand {
+		margin-left: -2px;
+		margin-right: 9px;
+	}
+	.select_old {
+		color: $text-lighten;
 	}
 </style>
