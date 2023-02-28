a024f77cd161065ba416e38d7d2bbad9df7a12d9
Sergey Zhitenev <s.zhitenev@finmars.com>
2023-02-28 13:10:45 +0000

    Update src/pages/settings/permissions/index.vue

--- src/pages/settings/permissions/index.vue
+++ src/pages/settings/permissions/index.vue
@@ -147,7 +147,7 @@
 		if ( !isConfirm ) return false
 
 		let res = await useApi('memberKick.post', {
-				params: {username: usernameDel, base_api_url: store.current.base_api_url}
+				body: {username: usernameDel, base_api_url: store.current.base_api_url}
 			})
 
 		// if ( stockMembers.value.find(item => item.username == usernameDel ) ) {
