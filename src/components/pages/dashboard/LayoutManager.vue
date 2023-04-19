876c89db516e3d6254630abd1cde3e56bc2d071d
Andrei Priakhin <strelok1415@gmail.com>
2023-04-19 21:42:14 +0400

    FN-1465

---
+++ src/components/pages/dashboard/LayoutManager.vue
@@ -0,0 +1,52 @@
+<template>
+	<BaseLayoutsManager
+		:layouts="dashStore.layoutList"
+		:isLayoutDefault="isLayoutDefault"
+
+
+	/>
+	<!-- @createNewLayout="createNewLayout"
+		@save="saveLayout"
+		@saveAs="openSaveAsModal = true"
+		@setAsDefault="setAsDefault"
+		@rename="renameLayout"
+		@delete="deleteLayout"
+		@export="openLayoutExport" -->
+</template>
+
+<script setup>
+
+	const router = useRouter();
+	const route = useRoute();
+	const dashStore = useStoreDashboard();
+
+	let props = defineProps({
+		layouts: Array,
+		autosaveLayout: Object,
+		loadingLayout: Boolean,
+		loadingLayoutsList: Boolean,
+
+		isLayoutDefault: Function
+	});
+
+
+	let emit = defineEmits([
+		'createNewLayout',
+		'setAsDefault',
+		'save',
+		'saveAs',
+		'export',
+		'openLayoutsList',
+		'openInvites',
+		'rename',
+		'delete',
+	]);
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
