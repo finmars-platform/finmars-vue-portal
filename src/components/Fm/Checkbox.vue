c579d2fb394db51d66a9eb2dc229221d7eeeb3d6
Andrei Priakhin <strelok1415@gmail.com>
2023-06-30 21:44:08 +0400

    FN-1672, FN-1709

--- src/components/Fm/Checkbox.vue
+++ src/components/Fm/Checkbox.vue
@@ -4,9 +4,11 @@
 			type="checkbox"
 			:checked="modelValue"
 			:disabled="disabled"
-    	@change="disabled || emit('update:modelValue', !modelValue)"
+			@change="disabled || emit('update:modelValue', !modelValue)"
 		/>
-		<span class="check" :class="{disabled: disabled}"><FmIcon class="mark" icon="check" :disabled="disabled" /></span>
+		<span class="check" :class="{ disabled: disabled }">
+			<FmIcon class="mark" icon="check" :disabled="disabled" />
+		</span>
 
 		<div v-if="label">{{ label }}</div>
 	</label>
@@ -21,54 +23,53 @@
 		tooltip: String,
 	})
 	let emit = defineEmits({
-		'update:modelValue': value => true,
+		'update:modelValue': (value) => true,
 	})
 </script>
 
 <style lang="scss" scoped>
+	input[type='radio'],
+	input[type='checkbox'] {
+		display: none;
+	}
+	input[type='radio']:checked + .check > .mark,
+	input[type='checkbox']:checked + .check > .mark {
+		opacity: 1;
+	}
+	input[type='checkbox']:checked + .check {
+		background: $primary;
+		border: none;
 
-input[type="radio"],
-input[type="checkbox"] {
-	display: none;
-}
-input[type="radio"]:checked + .check > .mark,
-input[type="checkbox"]:checked + .check > .mark  {
-	opacity: 1;
-}
-input[type="checkbox"]:checked + .check {
-	background: $primary;
-	border: none;
-
-	&.disabled {
-		background: #747474;
-		color: #fff;
+		&.disabled {
+			background: #747474;
+			color: #fff;
+		}
 	}
-}
-.check {
-	display: inline-block;
-	position: relative;
-	vertical-align: middle;
-	min-width: 20px;
-	height: 20px;
-	background: #fff;
-	border: 1px solid $border-darken;
-	margin-right: 10px;
-	cursor: pointer;
-	.circle {
-		border-radius: 50%;
+	.check {
+		display: inline-block;
+		position: relative;
+		vertical-align: middle;
+		min-width: 20px;
+		height: 20px;
+		background: #fff;
+		border: 1px solid $border-darken;
+		margin-right: 10px;
+		cursor: pointer;
+		.circle {
+			border-radius: 50%;
+		}
+		.mark {
+			display: block;
+			position: absolute;
+			top: 2px;
+			left: 2px;
+			opacity: 0;
+			transition: 0.3s;
+			font-size: 16px;
+			color: $separ;
+		}
+		.disabled {
+			cursor: default;
+		}
 	}
-	.mark {
-		display: block;
-		position: absolute;
-		top: 2px;
-		left: 2px;
-		opacity: 0;
-		transition: .3s;
-		font-size: 16px;
-		color: $separ;
-	}
-	.disabled {
-		cursor: default;
-	}
-}
 </style>
