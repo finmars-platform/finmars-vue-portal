5eb056a03e08fb5bda73a19d687162216b236c73
Andrei Priakhin <strelok1415@gmail.com>
2023-07-03 00:46:57 +0400

    FN-1803

---
+++ src/components/common/FinmarsGrid.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class="report-viewer-holder height-100">
+		<AngularFmGridTable
+			v-if="vm && vm.readyStatus.attributes && vm.readyStatus.layout"
+			class="g-group-table-holder"
+			:attributeDataService="vm.attributeDataService"
+			:evDataService="vm.entityViewerDataService"
+			:evEventService="vm.entityViewerEventService"
+			:spExchangeService="vm.splitPanelExchangeService"
+			:vm="vm"
+		/>
+		<div
+			v-else
+			class="e-data-loader"
+			layout="row"
+			layout-sm="column"
+			layout-align="space-around"
+		>
+			<FmLoader></FmLoader>
+		</div>
+	</div>
+</template>
+
+<script setup>
+	import reportViewerController from '@/angular/controllers/entityViewer/reportViewerController'
+
+	const route = {
+		current: {
+			name: 'app.portal.reports.profit-and-lost',
+		},
+		params: useRoute().query,
+	}
+
+	window.$state = route
+
+	// Modal hack
+	window.$mdDialog = {
+		modals: reactive({}),
+		show(opts) {
+			return new Promise((resolve, reject) => {
+				window.$mdDialog.modals[opts.controller.replace(' as vm', '')] = {
+					resolve,
+					reject,
+					...opts.locals,
+				}
+			})
+		},
+	}
+	provide('$mdDialog', window.$mdDialog)
+
+	let vm = ref(null)
+
+	let $scope = {
+		contentType: 'reports.plreport',
+	}
+
+	onMounted(() => {
+		vm.value = new reportViewerController({
+			$scope,
+			$stateParams: route.params,
+			route,
+		})
+		console.log('vm.value:', vm.value)
+	})
+</script>
+
+<style lang="scss" scoped></style>
