dca18c122c6c56dab2365a01ce8c046e5fb03c71
fyl1 <38299430+fyl1@users.noreply.github.com>
2023-08-16 17:00:42 +0300

    delete console

--- src/pages/settings/aliases/index.vue
+++ src/pages/settings/aliases/index.vue
@@ -511,25 +511,9 @@
 	const ecosystemDefaults = ref('local.poms.space0crgw')
 	const BaseInputEcosystemDefaults = ref([])
 
-	// configurationDefaultsGet()
-	// async function configurationDefaultsGet() {
-	// 	let edRes = await useApi('configurationList.get')
-	// 	configurationListItems.value = edRes.error ? {} : edRes.results
-	// 	// console.log(
-	// 	// 	'edRes',
-	// 	// 	edRes,
-	// 	// 	'configurationListItems',
-	// 	// 	configurationListItems.value
-	// 	// )
-	// }
+
 	init()
-	// console.log(
-	// 	store.defaultConfigCode,
-	// 	'store.defaultConfigCode',
-	// 	configurationListItems.value,
-	// 	'configurationListItems.value',
-	// 	configurationListItems.value.results
-	// )
+
 	async function init() {
 		const res = await Promise.all([
 			useApi('instrumentUserField.get', {
@@ -547,20 +531,12 @@
 		instrumentUserFieldItems.value = res[0].results
 		complexTransactionUserFieldItems.value = res[1].results
 		transactionUserFieldItems.value = res[2].results
-		// console.log(
-		// 	'complexTransactionUserFieldItems.value',
-		// 	complexTransactionUserFieldItems.value,
-		// 	'configurationListActive.value',
-		// 	configurationListActive.value
-		// )
+
 		complexTransactionUserFieldGet()
 		async function complexTransactionUserFieldGet() {
-			// console.log('textField до фильтрации совсем',complexTransactionUserFieldItems.value )
 
 			complexTransactionUserFieldItems.value.forEach(function (field) {
-				// console.log('textField до фильтрации', complexTransactionUserFieldItems.value )
 				textComplexTransactionUserFieldItems.forEach(function (textField) {
-					// console.log('textField при фильтрации', textField)
 					if (textField.key == field.key && field.key.includes('user_text')) {
 						textField.is_active = field.is_active
 						textField.name = field.name
@@ -569,7 +545,6 @@
 						textField.user_code = `${configurationListActive.value}:${field.key}`
 					}
 				})
-				// console.log('textComplexTransactionUserFieldItems', textComplexTransactionUserFieldItems)
 				numberComplexTransactionUserFieldItems.forEach(function (numberField) {
 					if (
 						numberField.key == field.key &&
@@ -631,14 +606,7 @@
 				})
 			})
 		}
-		// console.log(
-		// 	'instrumentUserFieldItems.value',
-		// 	transactionUserFieldItems.value
-		// )
-		// console.log(
-		// 	'textComplexTransactionUserFieldItems',
-		// 	textComplexTransactionUserFieldItems
-		// )
+	
 	}
 
 	watch(
@@ -661,7 +629,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -675,7 +642,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -692,7 +658,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -706,7 +671,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -723,7 +687,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -737,7 +700,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -751,29 +713,14 @@
 
 	function complexTransactionUserFieldItemsCreate() {
 		textComplexTransactionUserFieldItems.forEach(function (textField) {
-			// console.log(
-			// 	'textField',
-			// 	textField,
-			// 	'textField.id',
-			// 	textField.id,
-			// 	!textField.id
-			// )
-			// console.log('textField', textField)
+
 			if (textField.id >= 0) {
 				let res = useApi('complexTransactionUserField.put', {
 					params: { id: textField.id },
 					body: textField,
 				})
-				// console.log(
-				// 	'внутри',
-				// 	'textField',
-				// 	textField,
-				// 	'textField.id',
-				// 	textField.id,
-				// 	!textField.id
-				// )
+			
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -783,15 +730,11 @@
 			} else {
 				textField.configuration_code = configurationListActive.value
 				textField.user_code = `${configurationListActive.value}:${textField.key}`
-				console.log(
-					'textField complexTransactionUserField.post',
-					textField
-				)
+				
 				let res = useApi('complexTransactionUserField.post', {
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -808,7 +751,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -822,7 +764,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -839,7 +780,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -853,7 +793,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
@@ -873,7 +812,6 @@
 					body: textField,
 				})
 				if (res.error) {
-					// console.error(res.error);
 					useNotify({
 						type: 'error',
 						title: res.error.message || res.error.detail,
