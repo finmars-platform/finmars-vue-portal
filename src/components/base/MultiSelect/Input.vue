0583aa9f783f472e13dc97da0ea439e4d0fedd42
Andrei Priakhin <strelok1415@gmail.com>
2023-08-21 12:49:18 +0400

    FN-2004

--- src/components/base/MultiSelect/Input.vue
+++ src/components/base/MultiSelect/Input.vue
@@ -1,282 +1,181 @@
 <template>
 	<div>
-		<BaseInput class="ms_wrap"
-							 :label="title"
-							 @click="isOpen = true"
-							 modelValue=" "
+		<BaseInput
+			class="ms_wrap"
+			:label="title"
+			@click="isOpen = true"
+			modelValue=" "
 		>
 			<template #button><FmIcon icon="menu" /></template>
 
-			<div class="flex aic" style="height: inherit;">
-				<div class="flex-row fi-center fm_chip"
-						 v-for="item in selectedList"
-						 :key="selectedList[item_id]"
+			<div class="flex aic" style="height: inherit" ref="chipWrap">
+				<div
+					class="flex-row fi-center fm_chip"
+					v-for="item in selectedList"
+					:key="selectedList[item_id]"
 				>
-					<FmIcon v-if="item.error_data"
-									error
-									icon="info"
-									size="16"
-									class="m-r-8"
-									style="height: 16px;" />
-					<span>{{ item[item_title] }}</span>
+					<FmIcon
+						v-if="item.error_data"
+						error
+						icon="info"
+						size="16"
+						class="m-r-8"
+						style="height: 16px"
+					/>
+					<span
+						style="
+							white-space: nowrap;
+							overflow: hidden;
+							text-overflow: ellipsis;
+							display: inline-block;
+							width: calc(100% - 20px);
+							font-size: 12px;
+						"
+						>{{ item[item_title] }}</span
+					>
+
+					<FmIcon
+						icon="close"
+						size="16"
+						class="m-l-4"
+						@click.stop="deleteItem(item)"
+					/>
 				</div>
 			</div>
 		</BaseInput>
 
-		<!--		<BaseModal :title="title" v-model="isOpen"
-									 @cancel="isOpen = false">
-					<div class="flex sp aic">
-						<div class="available">
-							<div class="header">Available</div>
-
-							<div class="block">
-								<div class="search">
-									<BaseInput class="m-b-0"
-										label="Search"
-										v-model="availableSearch"
-									/>
-								</div>
-								<div class="list">
-									<div class="list_item"
-										v-for="item in availableList"
-										:key="item.name"
-										:class="{selected: item.selected}"
-										@click="item.selected = !item.selected"
-									>
-										{{ item[item_title] }}
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div class="actions">
-							<FmIcon btn icon="chevron_right" @click="addItem()" />
-							<FmIcon btn icon="keyboard_double_arrow_right" @click="addItem('all')" />
-
-							<FmIcon btn icon="chevron_left" @click="removeItem()" />
-							<FmIcon btn icon="keyboard_double_arrow_left" @click="removeItem('all')" />
-						</div>
-
-						<div class="selected">
-							<div class="header">Selected</div>
-
-							<div class="block">
-								<div class="search">
-									<BaseInput class="m-b-0"
-										label="Search"
-										v-model="selectedSearch"
-									/>
-								</div>
-								<div class="list">
-									<div class="list_item"
-										v-for="item in filteredSelList"
-										:key="item.name"
-										:class="{selected: item.selected}"
-										@click="item.selected = !item.selected"
-									>
-										{{ item[item_title] }}
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<template #controls>
-						<div class="flex sb">
-							<FmBtn type="text" @click="isOpen = false">cancel</FmBtn>
-							<FmBtn @click="save()">save</FmBtn>
-						</div>
-					</template>
-				</BaseModal>-->
-		<BaseMultiSelectModal :title="title"
-													:items="items"
-													v-model:opened="isOpen"
-													v-model="props.modelValue"
-													:item_id="item_id"
-													:item_title="item_title"
-													@cancel="isOpen = false"
-													@save="save" />
+		<BaseMultiSelectModal
+			:title="title"
+			:items="items"
+			v-model:opened="isOpen"
+			v-model="props.modelValue"
+			:item_id="item_id"
+			:item_title="item_title"
+			@cancel="isOpen = false"
+			@save="save"
+		/>
 	</div>
 </template>
 
 <script setup>
-import {getSelectedFilter} from "./multiSelectHelper";
+	import { getSelectedFilter } from './multiSelectHelper'
 
-let props = defineProps({
-	items: {
-		type: Array,
-		default: []
-	},
-	/** Unique keys or objects of selected items as string separated by comma or inside an array  */
-	modelValue: {
-		type: [String, Array],
-		default: [],
-	},
-	title: String,
-	item_id: {
-		type: String,
-		default: 'user_code'
-	},
-	item_title: {
-		type: String,
-		default: 'name'
-	},
-})
-let emit = defineEmits(['update:modelValue'])
+	let props = defineProps({
+		items: {
+			type: Array,
+			default: [],
+		},
+		/** Unique keys or objects of selected items as string separated by comma or inside an array  */
+		modelValue: {
+			type: [String, Array],
+			default: [],
+		},
+		title: String,
+		item_id: {
+			type: String,
+			default: 'user_code',
+		},
+		item_title: {
+			type: String,
+			default: 'name',
+		},
+	})
+	let emit = defineEmits(['update:modelValue'])
 
-let isOpen = ref(false)
-let availableSearch = ref('')
-let selectedSearch = ref('')
-/**
- * Set of unique ids of selected items
- * @type {Ref<Set>}
- * */
-let selectedFilter;
-/*let modelValueArray = props.modelValue
+	let isOpen = ref(false)
+	let chipWrap = ref(null)
+	let availableChips = ref(null)
 
-if ( typeof modelValueArray == 'string' ) modelValueArray = modelValueArray.split(',')
+	onMounted(() => {
+		availableChips.value = Math.floor(
+			chipWrap.value.getBoundingClientRect().width / 75
+		)
+	})
+	/**
+	 * Set of unique ids of selected items
+	 * @type {Ref<Set>}
+	 * */
+	let selectedFilter
 
-let selectedFilter = reactive( new Set( modelValueArray || [] ) )
+	selectedFilter = ref(getSelectedFilter(props.modelValue, props.item_id))
 
-let selectedFilterNames  = computed(() => {
-	return [...selectedFilter].map(
-		id => {
+	let selectedList = computed(() => {
+		let filtersArray = [...selectedFilter.value]
 
-			const selItem = props.items.find(item => item[props.item_id] === id);
+		if (!availableChips.value) return false
 
-			if (selItem) {
-				return selItem[props.item_title];
-			}
-
-			return '';
-
+		if (availableChips.value < filtersArray.length) {
+			filtersArray = filtersArray.slice(0, availableChips.value - 1)
 		}
-	)
-})*/
 
-/*function getSelectedFilter () {
+		let result = filtersArray.map((selId) => {
+			let selItem = props.items.find((item) => item[props.item_id] === selId)
 
-	let modelValueArray = props.modelValue;
-
-	if ( typeof modelValueArray === 'string' ) { // if selected items in form of string, convert them into array
-		modelValueArray = modelValueArray.split(',');
-	}
-
-	if (Array.isArray(modelValueArray) && typeof modelValueArray[0] === 'object') {
-		modelValueArray = modelValueArray.map(selItem => selItem[props.item_id]);
-	}
-
-	return new Set( modelValueArray || [] );
-
-}*/
-
-selectedFilter = ref(getSelectedFilter(props.modelValue, props.item_id));
-
-let selectedList = computed(() => {
-
-	return [...selectedFilter.value].map(selId => {
-		let selItem = props.items.find(item => item[props.item_id] === selId);
-
-		if (!selItem) {
-
-			return {
-				[props.item_id]: selId,
-				[props.item_title]: 'Not found',
-				error_data: {
-					description: ''
+			if (!selItem) {
+				return {
+					[props.item_id]: selId,
+					[props.item_title]: 'Not found',
+					error_data: {
+						description: '',
+					},
 				}
 			}
 
+			return selItem
+		})
+
+		if (availableChips.value < selectedFilter.value.size) {
+			result.push({
+				[props.item_id]: -1,
+				[props.item_title]:
+					selectedFilter.value.size - availableChips.value + 1 + '+',
+			})
 		}
 
-		return selItem;
-
+		return result
 	})
 
-});
-
-/*let filteredSelList = computed(() => {
-	return selectedList.value.filter(selItem => {
-		return selItem[props.item_title].toLocaleLowerCase().includes( selectedSearch.value.toLocaleLowerCase() )
-	})
-});
-
-let availableList = computed(() => {
-	// if ( !props.items.length ) return []
-
-	 return props.items.filter(
-			item => {
-
-				let itemId = item[props.item_id];
-
-				let itemPassesFilter = item[props.item_title].toLocaleLowerCase().includes(availableSearch.value.toLocaleLowerCase());
-
-				return !selectedFilter.value.has(itemId) && itemPassesFilter;
-
-			}
-	)
-
-})
-
-function addItem( all ) {
-	let items = availableList.value
-		.filter(item => item.selected || !!all)
-		.map( item => {
-			item.selected = false
-			return item[props.item_id]
-		})
-	items.forEach( item => selectedFilter.value.add(item) )
-}
-function removeItem( all ) {
-	let items = selectedList.value
-		.filter(item => item.selected || !!all)
-		.map( item => {
-			item.selected = false
-			return item[props.item_id]
-		})
-
-	items.forEach( item => selectedFilter.value.delete(item) )
-}
-
-function save() {
-	let result = [...selectedFilter.value]
-	if ( typeof props.modelValue == 'String' ) result = result.join(',')
-
-	emit('update:modelValue', result )
-
-	isOpen.value = false
-}*/
-
-function save(newValue) {
-	isOpen.value = false;
-	emit('update:modelValue', newValue );
-}
-
-watch(
-	() => props.modelValue,
-	() => {
-		selectedFilter.value = getSelectedFilter(props.modelValue, props.item_id);
+	function save(newValue) {
+		isOpen.value = false
+		emit('update:modelValue', newValue)
 	}
-)
 
+	function deleteItem(item) {
+		let index = props.modelValue.findIndex((o) => o == item.id)
+
+		props.modelValue.splice(index, 1)
+
+		let newRes = JSON.parse(JSON.stringify(props.modelValue))
+
+		emit('update:modelValue', newRes)
+	}
+
+	watch(
+		() => props.modelValue,
+		() => {
+			console.log('selectedFilter.value:', selectedFilter.value)
+			selectedFilter.value = getSelectedFilter(props.modelValue, props.item_id)
+		}
+	)
 </script>
 
 <style lang="scss" scoped>
-.ms_wrap {
-	cursor: pointer;
-}
-.fm_chip {
-	background: $main-darken-2;
-	padding: 3px 8px;
-	border-radius: 16px;
-	min-width: 30px;
-	text-align: center;
-
-	& + & {
-		margin-left: 5px;
+	.ms_wrap {
+		cursor: pointer;
 	}
-}
-/*.header {
+	.fm_chip {
+		background: $main-darken-2;
+		padding: 3px 8px;
+		border-radius: 16px;
+		min-width: 30px;
+		text-align: center;
+		max-width: 70px;
+
+		& + & {
+			margin-left: 5px;
+		}
+	}
+	/*.header {
 	font-size: 20px;
 	margin-bottom: 8px;
 }
