b97ffc50d7d77e6edcfacda676aa946210821f67
fyl1 <38299430+fyl1@users.noreply.github.com>
2023-08-31 11:26:20 +0300

    work on FN-1999

---
+++ src/components/modal/UserAttributes.vue
@@ -0,0 +1,152 @@
+<template>
+	<BaseModal
+		:modelValue="modelValue"
+		@update:modelValue="(newVal) => emit('update:modelValue', newVal)"
+		class="modal--rename"
+	>
+		<div>
+			<!-- TODO: use UserCode component -->
+			<!--			<FmInputText label="User Code"
+									 :errorData="nucErrorData"
+									 :modelValue="newUserCode"
+									 @update:modelValue="onUserCodeChange" />-->
+			<FmInputUserCode
+				v-model="newUserCode"
+				v-model:configuration_code="configCode"
+				:content_type="content_type"
+				v-model:errorData="nucErrorData"
+			/>
+			<FmInputText label="Attribute name" v-model="newName" />
+			<FmInputText label="Tooltip " />
+			<div class="checkbox-wrapp">
+				<FmCheckbox
+					v-model="multiselectIsActive"
+					tabindex="0"
+					label="Can recalculate"
+				/>
+			</div>
+			<FmInputText label="Attribute type" />
+			<div class="checkbox-wrapp">
+				<FmCheckbox
+					v-model="multiselectIsActive"
+					tabindex="0"
+					label="Is hidden"
+				/>
+			</div>
+		</div>
+		<!-- @update:model-value="toggleMultiselect(multiselectIsActive)" -->
+
+		<template #controls="{ cancel }">
+			<div class="flex-row fc-space-between">
+				<FmBtn type="basic" @click="() => cancelModal(cancel)">CANCEL</FmBtn>
+				<FmBtn type="basic" :disabled="!!nucErrorData" @click="save()"
+					>Make a copy</FmBtn
+				>
+				<FmBtn type="primary" :disabled="!!nucErrorData" @click="save()"
+					>SAVE</FmBtn
+				>
+				<FmBtn type="primary" :disabled="!!nucErrorData" @click="save()"
+					>Create</FmBtn
+				>
+			</div>
+		</template>
+	</BaseModal>
+</template>
+
+<script setup>
+	let props = defineProps({
+		modelValue: Boolean,
+		name: String,
+		user_code: String,
+		content_type: String,
+		occupiedUserCodes: {
+			type: Array,
+			default() {
+				return []
+			},
+		},
+	})
+
+	let emit = defineEmits(['save', 'update:modelValue'])
+
+	let newName = ref(props.name)
+	let newUserCode = ref(props.user_code)
+	let configCode = ref('')
+	let nucErrorData = ref(null)
+
+	watch(
+		() => props.name,
+		() => (newName.value = props.name)
+	)
+	watch(
+		() => props.user_code,
+		() => (newUserCode.value = props.user_code)
+	)
+
+	function onUserCodeChange(newVal) {
+		if (!newVal) {
+			nucErrorData.value = {
+				message: 'User code should not be empty',
+			}
+		} else if (
+			!nucErrorData.value &&
+			props.occupiedUserCodes.includes(newVal)
+		) {
+			nucErrorData.value = {
+				message: 'User code is occupied enter another one.',
+			}
+		} else if (
+			nucErrorData.value &&
+			!props.occupiedUserCodes.includes(newVal)
+		) {
+			nucErrorData.value = null
+		}
+
+		newUserCode.value = newVal
+	}
+
+	function save() {
+		if (!newUserCode.value) {
+			nucErrorData.value = {
+				message: 'User code should not be empty',
+			}
+		} else {
+			emit('save', {
+				name: newName.value,
+				user_code: newUserCode.value,
+				configuration_code: configCode.value,
+			})
+		}
+	}
+
+	function cancelModal(cancelFn) {
+		newName.value = props.name
+		newUserCode.value = props.user_code
+
+		cancelFn()
+	}
+</script>
+
+<style lang="scss">
+	.modal.modal--rename {
+		min-width: 600px;
+		& .bi_main_input {
+			color: #101010;
+		}
+		.selected_text {
+			color: #101010;
+		}
+		.usercode-input {
+			border: none;
+			padding: 0;
+			padding-bottom: 15px;
+		}
+		.bi_side_items {
+			display: flex;
+			padding: 0 8px;
+		}
+	}
+	.checkbox-wrapp {
+		margin-bottom: 20px;
+	}
+</style>
---
+++ src/components/modal/Warning.vue
@@ -0,0 +1,33 @@
+<template>
+	<BaseModal :title="title">
+
+		<div style="padding: 5px 0 20px;">
+			Are you sure to delete attribute Client group ?
+		</div>
+
+		<template #controls="{ cancel }">
+
+			<slot name="controls" :cancel="cancel">
+				<div class="btn-wrapp" >
+					<FmBtn type="basic" @click="cancel">CANCEL</FmBtn>
+                    <FmBtn type="basic" @click="cancel">OK</FmBtn>
+				</div>
+			</slot>
+
+		</template>
+	</BaseModal>
+</template>
+
+<script setup>
+	let props = defineProps({
+		title: String,
+		description: String,
+	})
+</script>
+
+<style lang="scss" scoped>
+.btn-wrapp{
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
