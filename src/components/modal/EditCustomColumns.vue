a6895a5ab4c94ff8e16d784e30df5d245f4871a5
fyl1 <38299430+fyl1@users.noreply.github.com>
2023-09-18 23:09:29 +0300

    work on FN-2097

--- src/components/modal/EditCustomColumns.vue
+++ src/components/modal/EditCustomColumns.vue
@@ -1,8 +1,14 @@
 <template>
 	<BaseModal :title="title">
 		<div class="wrapp">
-			<FmInputText label="Custom Column Name" />
-			<FmInputText label="Custom Column Reference Code (use programming language naming rules)" />
+			<FmInputText label="Custom Column Name" v-model="newName" />
+			<FmInputText
+				label="Custom Column Reference Code (use programming language naming rules)"
+				v-model="newUserCode"
+				v-model:configuration_code="configCode"
+				:content_type="content_type"
+				v-model:errorData="nucErrorData"
+			/>
 			<FmSelect label="Value type"></FmSelect>
 			<textarea name="" id="" cols="30" rows="10"></textarea>
 			<FmInputText label="Custom Column Expression" />
@@ -14,9 +20,10 @@
 
 		<template #controls="{ cancel }">
 			<slot name="controls" :cancel="cancel">
-				<div>
-					<FmBtn type="basic" @click="cancel">CANCEL</FmBtn>
-				</div>
+				<FmBtn type="basic" @click="() => cancelModal(cancel)">CANCEL</FmBtn>
+				<FmBtn type="primary" :disabled="!!nucErrorData" @click="save()"
+					>Save</FmBtn
+				>
 			</slot>
 		</template>
 	</BaseModal>
@@ -25,13 +32,50 @@
 <script setup>
 	let props = defineProps({
 		title: String,
-		description: String,
+		modelValue: Boolean,
+		name: String,
+		user_code: String,
+		content_type: String,
 	})
+	let emit = defineEmits(['save', 'update:modelValue'])
+
+	let newName = ref(props.name)
+	let newUserCode = ref(props.user_code)
+	let configCode = ref('')
+	let nucErrorData = ref(null)
+	watch(
+		() => props.name,
+		() => (newName.value = props.name)
+	)
+	watch(
+		() => props.user_code,
+		() => (newUserCode.value = props.user_code)
+	)
+	function save() {
+		if (!newUserCode.value) {
+			nucErrorData.value = {
+				message: 'User code should not be empty',
+			}
+		} else {
+			emit('save', {
+				name: newName.value,
+				user_code: newUserCode.value,
+				configuration_code: configCode.value,
+			})
+		}
+	}
+
+	function cancelModal(cancelFn) {
+		newName.value = props.name
+		newUserCode.value = props.user_code
+
+		cancelFn()
+	}
 </script>
 
 <style lang="scss" scoped>
-.wrapp{
-	padding: 0px 15px;
-	min-width: 500px;
-
-}</style>
+	.wrapp {
+		padding: 0px 15px;
+		min-width: 500px;
+	}
+</style>
