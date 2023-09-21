8ce858a873631252bfd7e6fa9fea1186d02afdc7
fyl1 <38299430+fyl1@users.noreply.github.com>
2023-09-21 11:59:02 +0300

    work on FN-2097

--- src/pages/portfolio-bundles/index.vue
+++ src/pages/portfolio-bundles/index.vue
@@ -7,8 +7,8 @@
 					<th>Name</th>
 					<th>Unique Code</th>
 					<th>Notes</th>
-					<th>Default Instrument Pricing Scheme</th>
-					<th>Default Currency Pricing Scheme</th>
+					<!-- <th>Default Instrument Pricing Scheme</th>
+					<th>Default Currency Pricing Scheme</th> -->
 					<th></th>
 				</tr>
 			</thead>
@@ -20,17 +20,25 @@
 					<!-- <td>{{ item.default_instrument_pricing_scheme_object.name }}</td> -->
 					<!-- <td>{{ item.default_currency_pricing_scheme_object.name }}</td>  -->
 					<td>
-						<FmBtn type="text" class="g-toggle-filters-btn">Edit </FmBtn>
-                        <FmBtn type="text" class="g-toggle-filters-btn"> Delete </FmBtn>
+						<FmBtn type="text" class="g-toggle-filters-btn" @click="editPricingPolicy(item)">Edit </FmBtn>
+                        <FmBtn type="text" class="g-toggle-filters-btn" @click="deletePricingPolicy(item)"> Delete </FmBtn>
 					</td>
 				</tr>
 			</tbody>
 		</table>
-		<FmBtn type="primary" class="g-toggle-filters-btn">
+		<FmBtn type="primary" class="g-toggle-filters-btn" @click="createPricingPolicy(item)">
 			<!-- @click="defaultSettingsCreate()"
 			:disabled="disabledBtn" -->
 			Add New
 		</FmBtn>
+		<ModalPortfolioBundleManager
+				title="Portfolio Bundle Manager"
+				v-model="isOpenEditPricingPolicy"
+				
+			></ModalPortfolioBundleManager>
+			<!-- :name="activeCustomColumns.name"
+				:user_code="activeCustomColumns.user_code"
+				@save="renameLayout" -->
 	</div>
 </template>
 
@@ -45,13 +53,60 @@
 		],
 	})
 	const pricingPolicyList = ref([])
-
+	let activePolicyList = ref([])
+	let isOpenEditPricingPolicy  = ref(false)
 	defaultsGet()
 	async function defaultsGet() {
-		let edRes = await useApi('pricingPolicyList.get')
+		let edRes = await useApi('portfolioBundles.get')
+		// let edRes = await useApi('pricingPolicyList.get')
+		
 		pricingPolicyList.value = edRes.error ? {} : edRes.results
 	}
-	console.log('pricingPolicyList', pricingPolicyList)
+	console.log('portfolioBundles', pricingPolicyList)
+	async function deletePricingPolicy(item) {
+		console.log('itemitem', item)
+		let confirm = await useConfirm({
+			title: 'Confirm action',
+			text: `Do you want to delete "${item.name}" layout?`,
+		})
+
+		if (confirm) {
+			deletePricingPolicyItem(item)
+		}
+	}
+
+	function deletePricingPolicyItem(item) {
+		let res = useApi('portfolioBundles.delete', {
+			params: { id: item.id },
+			body: item,
+		})
+		if (res.error) {
+			useNotify({
+				type: 'error',
+				title: res.error.message || res.error.detail,
+			})
+			throw new Error(res.error)
+		} else if (res.status === 'conflict') {
+			useNotify({
+				type: 'error',
+				title: 'You can not delete attributed that already in use',
+			})
+			throw new Error(res.error)
+		}
+		useNotify({ type: 'success', title: `data delete on the server` })
+		defaultsGet()
+	}
+	function editPricingPolicy(newNamesData) {
+		activePolicyList = newNamesData
+		// emit('rename', newNamesData)
+		isOpenEditPricingPolicy.value = true
+	}
+	function createPricingPolicy(newNamesData) {
+		activePolicyList = newNamesData
+		// emit('rename', newNamesData)
+		isOpenEditPricingPolicy.value = true
+	}
+	
 	// async function defaultSettingsCreate() {
 	// 	let res = await useApi('defaultSettings.put', {
 	// 		params: { id: ecosystemDefaults.value.id },
@@ -72,6 +127,7 @@
 <style lang="scss" scoped>
 	td,
 	th {
+		width: 25%;
 		padding: 0 5px;
 		display: table-cell;
 		text-align: left;
@@ -86,6 +142,7 @@
 		font-weight: 700;
 	}
     .portfolio-table{
+		width: 100%;
         margin-bottom: 40px;
     }
 	@media (max-width: 1200px) {
