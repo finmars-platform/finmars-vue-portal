0848a444d144f0c074439116c6832ee3764492f4
mevstratov <m.evstratov@finmars.com>
2023-11-28 20:37:30 +0400

    FN-2096 - fix after merging with develop

--- src/pages/portfolio-bundles/index.vue
+++ src/pages/portfolio-bundles/index.vue
@@ -2,190 +2,231 @@
 	<div class="container">
 		<table class="portfolio-table">
 			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Unique Code</th>
-					<th>Notes</th>
+			<tr>
+				<th>Name</th>
+				<th>Unique Code</th>
+				<th>Notes</th>
 
-					<th></th>
-				</tr>
+				<th></th>
+			</tr>
 			</thead>
 			<tbody>
-				<tr v-for="(item, index) in pricingPolicyList" :key="index">
-					<td>{{ item.name }}</td>
-					<td>{{ item.user_code }}</td>
-					<td>{{ item.notes }}</td>
+			<tr v-for="(item, index) in portfolioBundleList" :key="index">
+				<td>{{ item.name }}</td>
+				<td>{{ item.user_code }}</td>
+				<td>{{ item.notes }}</td>
 
-					<td>
-						<FmBtn
-							type="text"
-							class="g-toggle-filters-btn"
-							@click="editPricingPolicy(item)"
-							>Edit
-						</FmBtn>
-						<FmBtn
-							type="text"
-							class="g-toggle-filters-btn"
-							@click="deletePricingPolicy(item)"
-						>
-							Delete
-						</FmBtn>
-					</td>
-				</tr>
+				<td>
+					<FmBtn
+						type="text"
+						class="g-toggle-filters-btn"
+						@click="editPortfolioBundle(item)"
+					>Edit
+					</FmBtn>
+					<FmBtn
+						type="text"
+						class="g-toggle-filters-btn"
+						@click="deletePortfolioBundle(item)"
+					>
+						Delete
+					</FmBtn>
+				</td>
+			</tr>
 			</tbody>
 		</table>
 		<FmBtn
 			type="primary"
 			class="g-toggle-filters-btn"
-			@click="createPricingPolicy(item)"
+			@click="createPortfolioBundle(item)"
 		>
 			Add New
 		</FmBtn>
-		<div v-if="isOpenEditPricingPolicy">
+		<div v-if="isOpenEditPortfolioBundle">
 			<ModalPortfolioBundleManager
 				title="Portfolio Bundle Manager"
-				v-model="isOpenEditPricingPolicy"
-				:name="activePolicyList.name"
-				:user_code="activePolicyList.user_code"
-				:notes="activePolicyList.notes"
-				:typeModal="typeModal"
-				:registers="activePolicyList.registers"
+				v-model="isOpenEditPortfolioBundle"
+				:name="activePortfolioBundleList.name"
+				:user_code="activePortfolioBundleList.user_code"
+				:notes="activePortfolioBundleList.notes"
+				:сreation="сreation"
+				:publicName="activePortfolioBundleList.public_name"
+				:registers="activePortfolioBundleList.registers"
 				:registersItems="portfolioRegister"
+				:shortName="activePortfolioBundleList.short_name"
+				:modelValue="true"
 				@save="putEditPortfolioBundle"
 				@create="getCreatePortfolioBundle"
 			></ModalPortfolioBundleManager>
-		
 		</div>
 	</div>
 </template>
 
 <script setup>
-	definePageMeta({
-		middleware: 'auth',
-		bread: [
-			{
-				text: 'Default settings',
-				to: '/settings/default-settings',
-			},
-		],
+definePageMeta({
+	middleware: 'auth',
+	bread: [
+		{
+			text: 'Default settings',
+			to: '/settings/default-settings',
+		},
+	],
+})
+const portfolioBundleList = ref([])
+let activePortfolioBundleList = ref([])
+let isOpenEditPortfolioBundle = ref(false)
+let сreation = ref(false)
+let portfolioRegister = ref()
+
+defaultsGet()
+
+async function defaultsGet() {
+	let edRes = await useApi('portfolioBundles.get')
+
+	portfolioBundleList.value = edRes.error ? {} : edRes.results
+}
+
+async function deletePortfolioBundle(item) {
+
+	let confirm = await useConfirm({
+		title: 'Confirm action',
+		text: `Do you want to delete "${item.name}" layout?`,
 	})
-	const pricingPolicyList = ref([])
-	let activePolicyList = ref([])
-	let isOpenEditPricingPolicy = ref(false)
-	let typeModal = ref()
-	let portfolioRegister = ref()
+
+	if (confirm) {
+		deletePortfolioBundleItem(item)
+	}
+}
+
+getPortfolioRegister()
+
+async function getPortfolioRegister() {
+	let edRes = await useApi('portfolioRegisterList.get')
+	portfolioRegister.value = edRes.error ? {} : edRes.results
+}
+
+async function deletePortfolioBundleItem(item) {
+	let res = await useApi('portfolioBundles.delete', {
+		params: { id: item.id },
+		body: item,
+	})
+	if (res.error) {
+		useNotify({
+			type: 'error',
+			title: res.error.message || res.error.detail,
+		})
+		throw new Error(res.error)
+	} else if (res.status === 'conflict') {
+		useNotify({
+			type: 'error',
+			title: 'You can not delete attributed that already in use',
+		})
+		throw new Error(res.error)
+	}
+	useNotify({ type: 'success', title: `data delete on the server` })
 	defaultsGet()
-	async function defaultsGet() {
-		let edRes = await useApi('portfolioBundles.get')
+}
 
-		pricingPolicyList.value = edRes.error ? {} : edRes.results
-	}
-	console.log('portfolioBundles', pricingPolicyList)
-	async function deletePricingPolicy(item) {
-		console.log('itemitem', item)
-		let confirm = await useConfirm({
-			title: 'Confirm action',
-			text: `Do you want to delete "${item.name}" layout?`,
+function editPortfolioBundle(newNamesData) {
+	// activePortfolioBundleList = " "
+	activePortfolioBundleList = newNamesData
+	console.log('activePortfolioBundleснаружиs', activePortfolioBundleList)
+	сreation = false
+	console.log('сreation', сreation)
+	isOpenEditPortfolioBundle.value = true
+}
+function createPortfolioBundle(newNamesData) {
+	activePortfolioBundleList = ' '
+	console.log('activePortfolioBundleснаружиs2', activePortfolioBundleList)
+	сreation = true
+	console.log('сreation 2', сreation)
+
+	isOpenEditPortfolioBundle.value = true
+}
+async function putEditPortfolioBundle(newNamesData) {
+	// activePortfolioBundleList = []
+	console.log('putEditPortfolioBundle newNamesData', newNamesData)
+	let res = await useApi('portfolioBundles.put', {
+		params: { id: activePortfolioBundleList.id },
+		body: newNamesData,
+	})
+	if (res.error) {
+		useNotify({
+			type: 'error',
+			title: res.error.message || res.error.detail,
 		})
-
-		if (confirm) {
-			deletePricingPolicyItem(item)
-		}
-	}
-	
-	
-	async function getPortfolioRegister() {
-		let edRes = await useApi('portfolioRegisterList.get')
-		portfolioRegister.value = edRes.error ? {} : edRes.results
-	}
-	getPortfolioRegister()
-	console.log('portfolioRegister', portfolioRegister)
-	function deletePricingPolicyItem(item) {
-		let res = useApi('portfolioBundles.delete', {
-			params: { id: item.id },
-			body: item,
+		throw new Error(res.error)
+	} else if (res.status === 'conflict') {
+		useNotify({
+			type: 'error',
+			title: 'You can not Edit CustomColumns that already in use',
 		})
-		if (res.error) {
-			useNotify({
-				type: 'error',
-				title: res.error.message || res.error.detail,
-			})
-			throw new Error(res.error)
-		} else if (res.status === 'conflict') {
-			useNotify({
-				type: 'error',
-				title: 'You can not delete attributed that already in use',
-			})
-			throw new Error(res.error)
-		}
-		useNotify({ type: 'success', title: `data delete on the server` })
-		defaultsGet()
+		throw new Error(res.error)
 	}
-	function editPricingPolicy(newNamesData) {
-		activePolicyList = newNamesData
-		console.log('activePolicyList.registers', activePolicyList.registers)
-	
-		typeModal = 'edit'
-		isOpenEditPricingPolicy.value = true
-	}
-	function createPricingPolicy(newNamesData) {
-	
-		typeModal = 'create'
-		isOpenEditPricingPolicy.value = true
-	}
+	useNotify({ type: 'success', title: `data Edit on the server` })
+	defaultsGet()
 
-	// async function defaultSettingsCreate() {
-	// 	let res = await useApi('defaultSettings.put', {
-	// 		params: { id: ecosystemDefaults.value.id },
-	// 		body: ecosystemDefaults.value,
-	// 	})
+	isOpenEditPortfolioBundle.value = false
+}
 
-	// 	if (res.error) {
-	// 		// console.error(res.error);
-	// 		useNotify({ type: 'error', title: res.error.message || res.error.detail })
-	// 		throw new Error(res.error)
-	// 	} else {
-	// 		useNotify({ type: 'success', title: `data saved on the server` })
-	// 	}
-	// 	disabledBtn.value = true
-	// }
+async function getCreatePortfolioBundle(newNamesData) {
+	activePortfolioBundleList = {}
+	let res = await useApi('portfolioBundles.post', {
+		body: newNamesData,
+	})
+	if (res.error) {
+		useNotify({
+			type: 'error',
+			title: res.error.message || res.error.detail,
+		})
+		throw new Error(res.error)
+	} else if (res.status === 'conflict') {
+		useNotify({
+			type: 'error',
+			title: 'You can not Edit CustomColumns that already in use',
+		})
+		throw new Error(res.error)
+	}
+	useNotify({ type: 'success', title: `data Edit on the server` })
+	defaultsGet()
+	isOpenEditPortfolioBundle.value = false
+}
 </script>
 
 <style lang="scss" scoped>
-	td,
-	th {
-		width: 25%;
-		padding: 0 5px;
-		display: table-cell;
-		text-align: left;
-		vertical-align: middle;
-		border-radius: 2px;
+td,
+th {
+	width: 25%;
+	padding: 0 5px;
+	display: table-cell;
+	text-align: left;
+	vertical-align: middle;
+	border-radius: 2px;
+}
+.container {
+	padding: 30px;
+}
+.title {
+	margin-bottom: 24px;
+	font-weight: 700;
+}
+.portfolio-table {
+	width: 100%;
+	margin-bottom: 40px;
+}
+@media (max-width: 1200px) {
+	.wrapp-select {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		grid-template-rows: 1fr;
+		gap: 10px 5px;
 	}
-	.container {
-		padding: 30px;
+}
+@media (max-width: 767px) {
+	.wrapp-select {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 1fr;
+		gap: 10px 5px;
 	}
-	.title {
-		margin-bottom: 24px;
-		font-weight: 700;
-	}
-	.portfolio-table {
-		width: 100%;
-		margin-bottom: 40px;
-	}
-	@media (max-width: 1200px) {
-		.wrapp-select {
-			display: grid;
-			grid-template-columns: 1fr 1fr 1fr;
-			grid-template-rows: 1fr;
-			gap: 10px 5px;
-		}
-	}
-	@media (max-width: 767px) {
-		.wrapp-select {
-			display: grid;
-			grid-template-columns: 1fr;
-			grid-template-rows: 1fr;
-			gap: 10px 5px;
-		}
-	}
+}
 </style>
