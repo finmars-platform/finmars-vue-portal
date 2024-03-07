463cb396227fc1d91231ee9b5d57c3bba5582bb3
Sergey Zhitenev <szhitenev@ip-192-168-178-21.eu-central-1.compute.internal>
2024-03-08 00:44:13 +0100

    PLAT-482 fix

--- src/components/modal/PerformanceDetail.vue
+++ src/components/modal/PerformanceDetail.vue
@@ -1,59 +1,77 @@
 <template>
-	<BaseModal v-model="modelValue" :closingDisabled="creating" @update:modelValue="cancel">
+	<BaseModal v-model="modelValue" :closingDisabled="creating" @update:modelValue="cancel" :onShowModal="onShowModal">
 
 		<div style="padding: 16px;">
 			<div v-if="performanceDetailsColumnName!=='name'">
 				<table style="width: 100%;">
 					<tr>
 						<td>Date Start:</td>
-						<td class="text-right"><b>{{ performanceDetails.begin_date }}</b><FmCopyButton :text="performanceDetails.begin_date"></FmCopyButton></td>
+						<td class="text-right"><b>{{ performanceDetails.begin_date }}</b>
+							<FmCopyButton :text="performanceDetails.begin_date"></FmCopyButton>
+						</td>
 					</tr>
 					<tr>
 						<td>Date Start NAV:</td>
-						<td class="text-right"><b>{{ $format(performanceDetails.begin_nav) }}</b> <FmCopyButton :text="performanceDetails.begin_nav"></FmCopyButton></td>
+						<td class="text-right"><b>{{ $format(performanceDetails.begin_nav) }}</b>
+							<FmCopyButton :text="performanceDetails.begin_nav"></FmCopyButton>
+						</td>
 					</tr>
 					<tr>
 						<td>Date End:</td>
-						<td class="text-right"><b>{{ performanceDetails.end_date }}</b> <FmCopyButton :text="performanceDetails.end_date"></FmCopyButton></td>
+						<td class="text-right"><b>{{ performanceDetails.end_date }}</b>
+							<FmCopyButton :text="performanceDetails.end_date"></FmCopyButton>
+						</td>
 					</tr>
 					<tr>
 						<td>Date End NAV:</td>
-						<td class="text-right"><b>{{ $format(performanceDetails.end_nav) }}</b> <FmCopyButton :text="performanceDetails.end_nav"></FmCopyButton></td>
+						<td class="text-right"><b>{{ $format(performanceDetails.end_nav) }}</b>
+							<FmCopyButton :text="performanceDetails.end_nav"></FmCopyButton>
+						</td>
 					</tr>
 					<tr>
 						<td>Cash flow:</td>
-						<td class="text-right"><b>{{ $format(performanceDetails.grand_cash_flow) }}</b> <FmCopyButton :text="performanceDetails.grand_cash_flow"></FmCopyButton></td>
+						<td class="text-right"><b>{{ $format(performanceDetails.grand_cash_flow) }}</b>
+							<FmCopyButton :text="performanceDetails.grand_cash_flow"></FmCopyButton>
+						</td>
 					</tr>
 					<tr>
 						<td>Weighted Cash flow:</td>
-						<td class="text-right"><b>{{ $format(performanceDetails.grand_cash_flow_weighted) }}</b> <FmCopyButton :text="performanceDetails.grand_cash_flow_weighted"></FmCopyButton></td>
+						<td class="text-right"><b>{{ $format(performanceDetails.grand_cash_flow_weighted) }}</b>
+							<FmCopyButton :text="performanceDetails.grand_cash_flow_weighted"></FmCopyButton>
+						</td>
 					</tr>
 					<tr>
 						<td>Absolute P&L:</td>
-						<td class="text-right"><b>{{ $format(performanceDetails.grand_absolute_pl) }}</b> <FmCopyButton :text="performanceDetails.grand_absolute_pl"></FmCopyButton></td>
+						<td class="text-right"><b>{{ $format(performanceDetails.grand_absolute_pl) }}</b>
+							<FmCopyButton :text="performanceDetails.grand_absolute_pl"></FmCopyButton>
+						</td>
 					</tr>
 					<tr>
 						<td>Return:</td>
-						<td class="text-right"><b>{{parseFloat(performanceDetails.grand_return * 100).toFixed(2)}}%</b> <FmCopyButton :text="parseFloat(performanceDetails.grand_return * 100).toFixed(2)"></FmCopyButton></td>
+						<td class="text-right">
+							<b>{{ parseFloat(performanceDetails.grand_return * 100).toFixed(2) }}%</b>
+							<FmCopyButton
+								:text="parseFloat(performanceDetails.grand_return * 100).toFixed(2)"></FmCopyButton>
+						</td>
 					</tr>
 					<tr>
 						<td>Period Type:</td>
-						<td class="text-right"><b>{{performanceDetails.period_type}}</b></td>
+						<td class="text-right"><b>{{ performanceDetails.period_type }}</b></td>
 					</tr>
 					<tr>
 						<td>Calculation Type:</td>
-						<td class="text-right"><b>{{performanceDetails.calculation_type}}</b></td>
+						<td class="text-right"><b>{{ performanceDetails.calculation_type }}</b></td>
 					</tr>
 					<tr>
 						<td>Report Currency:</td>
-						<td class="text-right"><b>{{performanceDetails.report_currency_object.name}}</b></td>
+						<td class="text-right"><b>{{ performanceDetails.report_currency_object.name }}</b></td>
 					</tr>
 				</table>
 			</div>
-			<div v-else-if="performanceDetailsColumnName==='name'">
+			<div v-else-if="performanceDetailsColumnName === 'name'">
 				<table>
 					<tr v-show="readyStatus" v-for="portfolio in portfolios" :key="portfolio.id">
-						<td class="text-center" ><b>{{ portfolio.name }}</b></td>
+						<td class="text-center"><b>{{ portfolio.name }}</b></td>
 					</tr>
 				</table>
 			</div>
@@ -84,7 +102,7 @@ let portfolios = ref([])
 
 async function getDetailPortfolioBundle(bundleId) {
 	return await useApi('portfolioBundleRegistersList.get', {
-		params: { id: bundleId },
+		params: {id: bundleId},
 	})
 }
 
@@ -99,26 +117,41 @@ function cancel() {
 	emit('cancel')
 }
 
-watch(
-	() => props.performanceDetails,
-	() => {
-		if (props.performanceDetails !== null && props.performanceDetailsColumnName === "name"){
-			readyStatus.value = false;
-			getPortfolios().then(result => {
-				portfolios.value = result
-				readyStatus.value = true;
-			}).catch(error => {
-				console.error(error)
-			})
-		}
-	}
-)
-
 async function init() {
+
+	console.log("PerformanceDetail.init");
+
+	getPortfolios().then(result => {
+		portfolios.value = result
+		readyStatus.value = true;
+	}).catch(error => {
+		console.error(error)
+	})
+
+	watch(
+		() => props.performanceDetails,
+		() => {
+			if (props.performanceDetails !== null && props.performanceDetailsColumnName === "name") {
+				readyStatus.value = false;
+				getPortfolios().then(result => {
+					portfolios.value = result
+					readyStatus.value = true;
+				}).catch(error => {
+					console.error(error)
+				})
+			}
+		}
+	)
 
 }
 
-init()
+function onShowModal() {
+
+	init();
+
+}
+
+
 </script>
 
 <style lang="scss" scoped>
@@ -128,6 +161,7 @@ init()
 		margin-bottom: 0;
 	}
 }
+
 .text-right {
 	text-align: right;
 }
@@ -135,6 +169,7 @@ init()
 .text-center {
 	text-align: center;
 }
+
 .copy-button {
 	margin-left: 8px;
 	margin-bottom: 12px;
