8567b5f1f5cc8f2021cb247ec7069a2fd67b494d
mevstratov <m.evstratov@finmars.com>
2024-04-17 14:38:25 +0400

    Update vue and nuxt

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vitest/config'
-import commonjs from 'vite-plugin-commonjs'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-	plugins: [commonjs(/* options */)],
-})
