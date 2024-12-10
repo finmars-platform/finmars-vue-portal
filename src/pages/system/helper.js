60f299b453e036e5129592be5d7b8c8727705f31
Armen Khachatryan <a.khachatryan@finmars.com>
2024-12-10 14:59:54 +0400

    PLAT-892 - fixed comments

--- src/pages/system/helper.js
+++ src/pages/system/helper.js
@@ -17,3 +17,30 @@ export async function loadMultiselectOpts(
 	}
 	return [];
 }
+
+export function downloadFile(blobParts, blobType, downloadFileName) {
+	// Create a new blob object with the specified mime-type
+	const newBlob = new Blob([blobParts], { type: blobType });
+
+	// Check if we're in an IE environment and handle it accordingly
+	if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+		window.navigator.msSaveOrOpenBlob(newBlob);
+		return;
+	}
+
+	// For modern browsers: Create a link pointing to the ObjectURL containing the blob
+	const data = window.URL.createObjectURL(newBlob);
+	const link = document.createElement('a');
+	link.href = data;
+	link.download = downloadFileName;
+
+	// Append the link to the document and trigger the download
+	document.body.appendChild(link);
+	link.click();
+
+	// Clean up after the download is initiated
+	setTimeout(() => {
+		document.body.removeChild(link);
+		window.URL.revokeObjectURL(data);
+	}, 100);
+}
