1b3669cc2e5fb77c4e16342fc7077e3ab75e5569
Vitaly <vitaliy@uxpressia.com>
2024-12-20 22:13:43 +0300

    added the 'Calculated Columns' block on the 'Scheme' tab in the editor of the entity import scheme

--- src/components/modal/importSchemes/SimpleImportScheme/SimpleTabSchemeCalculated.vue
+++ src/components/modal/importSchemes/SimpleImportScheme/SimpleTabSchemeCalculated.vue
@@ -31,13 +31,21 @@
 						@change="updateField(element, 'name', $event)"
 					/>
 
-					<FmTooltip type="secondary" location="top">
+					<FmTooltip
+						type="secondary"
+						location="top"
+						:disabled="!element.name || !!element.name_expr"
+					>
 						<template #activator="{ props }">
 							<FmIconButton
+								class="calculated-block__item-expression"
 								v-ripple.center
 								icon="mdi-function-variant"
-								variant="outlined"
+								:variant="
+									element.name && !element.name_expr ? 'flat' : 'outlined'
+								"
 								v-bind="props"
+								@click.stop.prevent="openExpressionEditor(element)"
 							/>
 						</template>
 
@@ -64,6 +72,15 @@
 		>
 			Add field
 		</FmButton>
+
+		<ExpressionEditor
+			v-if="expressionEditor.show"
+			:rule-expr="expressionEditor.item?.name_expr"
+			:data="expressionEditorSelectorData"
+			disallow-empty-value
+			@close="expressionEditor.show = false"
+			@update="onExpressionEditorUpdate"
+		/>
 	</div>
 </template>
 
@@ -79,9 +96,10 @@
 		FmIconButton,
 		FmTextField,
 		FmTooltip,
-		getRandomString,
 		Ripple
 	} from '@finmars/ui';
+	import ExpressionEditor from '~/components/common/ExpressionEditorSelector/ExpressionEditor.vue';
+	import { getFunctions } from '~/components/modal/importSchemes/utils';
 
 	const vRipple = Ripple;
 
@@ -96,6 +114,11 @@
 
 	const emits = defineEmits(['update:block', 'update:valid']);
 
+	const expressionEditor = ref({
+		show: false,
+		item: null
+	});
+
 	const calculatedInputs = computed(() => {
 		if (isEmpty(props.scheme?.calculated_inputs)) {
 			return [];
@@ -106,12 +129,19 @@
 			.map((i) => ({
 				...i,
 				frontOptions: {
-					key: getRandomString(4),
 					validateError: {
-						name: getFieldValidationError(i, 'name')
+						name: getFieldValidationError(i, 'name'),
+						name_expr: getFieldValidationError(i, 'name_expr')
 					}
 				}
 			}));
+	});
+
+	const expressionEditorSelectorData = computed(() => {
+		return {
+			groups: [{ name: '<b>Imported</b>', key: 'input' }],
+			functions: [getFunctions(props.scheme.csv_fields)]
+		};
 	});
 
 	const isBlockValid = computed(
@@ -119,7 +149,7 @@
 			!calculatedInputs.value.some((i) => {
 				const { frontOptions = {} } = i;
 				const { validateError = {} } = frontOptions;
-				return !!validateError.name;
+				return !!validateError.name || !!validateError.name_expr;
 			})
 	);
 
@@ -127,19 +157,28 @@
 		return !item[fieldName] ? 'This field may not blank' : '';
 	}
 
+	function findItemIndex(item) {
+		return props.scheme.calculated_inputs.findIndex(
+			(i) => item.column === i.column
+		);
+	}
+
 	function onMoveItemEnd({ oldIndex, newIndex }) {
 		console.log('onMoveCalculatedItemEnd: ', oldIndex, newIndex);
+		const updatedBlock = cloneDeep(props.scheme.calculated_inputs);
+		const movedItems = updatedBlock.splice(oldIndex, 1);
+		updatedBlock.splice(newIndex, 0, movedItems[0]);
+		updatedBlock.forEach((i, index) => (i.column = index));
+		updatedBlock.sort((a, b) => a.column - b.column);
+		emits('update:block', updatedBlock);
 	}
 
 	function addField() {
-		console.log('addField (calculated_inputs)');
 		const updatedBlock = cloneDeep(props.scheme.calculated_inputs);
 		updatedBlock.push({
 			name: '',
-			column: size(calculatedInputs.value) + 1,
-			frontOptions: {
-				key: getRandomString(4)
-			}
+			name_expr: '',
+			column: size(calculatedInputs.value) + 1
 		});
 		emits('update:block', updatedBlock);
 	}
@@ -158,17 +197,40 @@
 	}
 
 	function updateField(item, field, value) {
-		console.log('updateField (calculated_inputs): ', item, field, value);
-		const index = props.scheme.calculated_inputs.findIndex(
-			(i) => i.id === item.id || i.frontOptions?.key === item.frontOptions?.key
-		);
+		const index = findItemIndex(item);
 		if (
 			index !== -1 &&
 			props.scheme.calculated_inputs[index][field] !== value
 		) {
 			const updatedBlock = cloneDeep(props.scheme.calculated_inputs);
 			updatedBlock[index][field] = value;
+			console.log('updateField: ', updatedBlock);
 			emits('update:block', updatedBlock);
+		}
+	}
+
+	function openExpressionEditor(item) {
+		expressionEditor.value = {
+			show: true,
+			item
+		};
+	}
+
+	function onExpressionEditorUpdate(value) {
+		const { item } = expressionEditor.value;
+		const index = findItemIndex(item);
+		if (
+			index !== -1 &&
+			props.scheme.calculated_inputs[index].name_expr !== value
+		) {
+			const updatedBlock = cloneDeep(props.scheme.calculated_inputs);
+			updatedBlock[index].name_expr = value;
+			console.log('onExpressionEditorUpdate: ', updatedBlock);
+			emits('update:block', updatedBlock);
+			expressionEditor.value = {
+				show: false,
+				item: null
+			};
 		}
 	}
 
@@ -221,6 +283,12 @@
 			&-del-btn {
 				position: absolute;
 				right: 4px;
+			}
+
+			&-expression {
+				&.v-btn--variant-flat {
+					background-color: var(--error) !important;
+				}
 			}
 		}
 
