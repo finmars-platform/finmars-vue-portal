233bd34616340ff64e9bdda1f573d0f0f06254b0
Armen Khachatryan <a.khachatryan@finmars.com>
2025-02-12 19:55:56 +0400

    PLAT-1462 - Add Schedule edit page

---
+++ src/pages/system/schedule/procedure/index.vue
@@ -0,0 +1,172 @@
+<template>
+	<div>
+		<div class="grid grid-cols-[40px_1fr_1fr_60px] gap-4 mb-2">
+			<div class="text-center">Order</div>
+			<div>Type</div>
+			<div>Code</div>
+			<div></div>
+		</div>
+
+		<draggable
+			v-model="currentItemProceduresList"
+			item-key="index"
+			@end="updateOrderNumbers"
+			:animation="200"
+			ghost-class="drag-ghost"
+			handle=".drag-handle"
+		>
+			<template #item="{ element: proc, index }">
+				<div class="grid grid-cols-[40px_1fr_1fr_60px] gap-4 items-center">
+					<div class="text-center mb-2 drag-handle cursor-grab">
+						<span>{{index+1}}</span>
+						<span class="ml-4 max-w-20">☰</span>
+					</div>
+					<div class="mb-2 select-wrapper">
+						<FmSelect
+							v-model="proc.type"
+							:options="proceduresTypeOptions"
+							@update:modelValue="(value) => changeOption(proc, value)"
+						/>
+					</div>
+					<div class="mb-2 select-wrapper">
+						<FmSelect
+							v-model="proc.user_code"
+							:options="getOptionsForRow(proc)"
+							@update:modelValue="updateItem"
+							:disabled="!proc.type"
+						/>
+					</div>
+					<div class="flex justify-center mb-2">
+						<FmButton type="secondary" @click="removeItem(proc)" rounded>
+							Delete
+						</FmButton>
+					</div>
+				</div>
+			</template>
+		</draggable>
+	</div>
+</template>
+
+<script setup>
+	import { ref } from 'vue';
+	import { FmButton, FmSelect } from '@finmars/ui';
+	import draggable from 'vuedraggable';
+
+	const props = defineProps({
+		procedures: Array
+	});
+
+	const pricingProcedures = ref([]);
+	const dataProcedures = ref([]);
+	const expressionProcedures = ref([]);
+
+	const currentItemProceduresList = ref([]);
+
+	const proceduresTypeOptions = [
+		{ title: 'Pricing Procedure', value: 'pricing_procedure' },
+		{ title: 'Data Procedure', value: 'data_procedure' },
+		{ title: 'Expression Procedure', value: 'expression_procedure' }
+	];
+
+	const emit = defineEmits(['removeItem', 'updateItem']);
+
+	const getOptionsForRow = (proc) => {
+		switch (proc.type) {
+			case 'pricing_procedure': return pricingProcedures.value;
+			case 'data_procedure': return dataProcedures.value;
+			case 'expression_procedure': return expressionProcedures.value;
+			default: return [];
+		}
+	};
+
+	const removeItem = (proc) => {
+		const index = currentItemProceduresList.value.indexOf(proc);
+		if (index !== -1) {
+			currentItemProceduresList.value.splice(index, 1);
+		}
+		emit('removeItem', currentItemProceduresList.value);
+	};
+
+	const changeOption = (proc, opt) => {
+		proc.type = opt;
+		proc.user_code = null;
+	};
+
+	const updateItem = () => {
+		emit('updateItem', currentItemProceduresList.value);
+	};
+
+	const updateOrderNumbers = () => {
+		currentItemProceduresList.value.forEach((proc, index) => {
+			proc.order = index + 1;
+		});
+		emit('updateItem', currentItemProceduresList.value);
+	};
+
+	const filteredProcedures = (item) => {
+		return item.map((el) => ({
+			title: `${el.name} (${el.user_code})`,
+			value: el.user_code
+		}));
+	};
+
+	async function getPricingProcedures() {
+		const res = await useApi('pricingProc.get');
+		if (res && res._$error) {
+			useNotify({ type: 'error', title: res._$error.message || res._$error.detail });
+		} else {
+			pricingProcedures.value = filteredProcedures(res.results);
+		}
+	}
+
+	async function getDataProcedures() {
+		const res = await useApi('importBankProc.get');
+		if (res && res._$error) {
+			useNotify({ type: 'error', title: res._$error.message || res._$error.detail });
+		} else {
+			dataProcedures.value = filteredProcedures(res.results);
+		}
+	}
+
+	async function getExpressionProcedures() {
+		const res = await useApi('expressionProcedureList.get');
+		if (res && res._$error) {
+			useNotify({ type: 'error', title: res._$error.message || res._$error.detail });
+		} else {
+			expressionProcedures.value = filteredProcedures(res.results);
+		}
+	}
+
+	async function init() {
+		currentItemProceduresList.value = props?.procedures;
+		await getPricingProcedures();
+		await getDataProcedures();
+		await getExpressionProcedures();
+	}
+
+	init();
+</script>
+
+<style scoped lang="scss">
+	.select-wrapper {
+		max-width: 100%;
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		margin: var(--spacing-8) 0;
+	}
+
+	/* Style for dragging */
+	.drag-ghost {
+		opacity: 0.5;
+		background: #f0f0f0;
+	}
+
+	.cursor-grab {
+		cursor: grab;
+	}
+
+	.cursor-grab:active {
+		cursor: grabbing;
+	}
+</style>
